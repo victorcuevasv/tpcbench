aae506f669eb440a60ed94a3433d1b3fae88298e
victorcuevasv <victorcuevasv@gmail.com>
2018-12-10 15:10:27 +0100

    Bug fix: creation if target/classes directory in the client.

--- client/compile.sh
+++ client/compile.sh
@@ -2,6 +2,12 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
+#Create the target/classes directory if it does not exist.
+
+if [ ! -d $DIR/project/target/classes ]; then
+   mkdir -p $DIR/project/target/classes
+fi
+
 #Build the project with the container using Maven by running the container (standalone, with no docker-compose).
 docker run --rm -v $DIR/project:/project  --entrypoint mvn  buildhiveclient:dev clean package -f /project/pom.xml   
 
