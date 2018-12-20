db3f007bafd7328d4f38f1e5f77b1240a5069f7e
victorcuevasv <victorcuevasv@gmail.com>
2018-12-20 18:31:57 +0100

    Simplify Java Spark-Hive client.

--- runclient_sparkexample.sh
+++ runclient_sparkexample.sh
@@ -2,3 +2,7 @@
 
 docker exec -ti  sparkhiveclientcontainer  /bin/bash -c "/opt/spark-2.4.0-bin-hadoop2.7/bin/run-example --master spark://sparkhiveservercontainer:7077 SparkPi 10"     
 
+#Run locally.
+
+#docker exec -ti  sparkhiveclientcontainer  /bin/bash -c "/opt/spark-2.4.0-bin-hadoop2.7/bin/run-example SparkPi 10"
+
--- runclient_sparkhiveexample.sh
+++ runclient_sparkhiveexample.sh
@@ -7,5 +7,9 @@
 
 #docker exec -ti  hiveclientcontainer  /bin/bash -c "mvn exec:java -Dexec.mainClass=\"org.bsc.dcc.vcv.ProcessCreateScript\" -Dexec.args=\"/data tpcds.sql\" -f /project/pom.xml"
 
-docker exec -ti  sparkhiveclientcontainer  /bin/bash -c "mvn exec:java -Dexec.mainClass=\"org.bsc.dcc.vcv.JavaSparkHiveExample\" -Dexec.args=\"\" -f /project/pom.xml"
+#docker exec -ti  sparkhiveclientcontainer  /bin/bash -c "mvn exec:java -Dexec.mainClass=\"org.bsc.dcc.vcv.JavaSparkHiveExample\" -Dexec.args=\"\" -f /project/pomSpark.xml"
+
+#Does not work.
+
+#docker exec -ti  sparkhiveclientcontainer  /bin/bash -c "/opt/spark-2.4.0-bin-hadoop2.7/bin/spark-submit --class org.bsc.dcc.vcv.JavaSparkHiveExample --master spark://sparkhiveservercontainer:7077 --deploy-mode cluster /temporal/client-1.0-SNAPSHOT-jar-with-dependencies.jar"       
 
