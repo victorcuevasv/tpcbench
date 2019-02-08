fa7387f6ca8da220fbf3dc1f3bd48b4234aac1ae
victorcuevasv <victorcuevasv@gmail.com>
2019-02-08 17:04:04 +0100

    Fix bug in logging.

--- client/project/src/main/java/org/bsc/dcc/vcv/QueryStreamSpark.java
+++ client/project/src/main/java/org/bsc/dcc/vcv/QueryStreamSpark.java
@@ -25,6 +25,8 @@ import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SaveMode;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class QueryStreamSpark implements Callable<Void> {
 
