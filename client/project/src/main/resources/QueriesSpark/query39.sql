d302f6dff99d5e4cab6505995aa55db9cda5f642
victorcuevasv <victorcuevasv@gmail.com>
2019-02-17 23:13:17 +0100

    Fix Spark query templates.

--- client/project/src/main/resources/QueriesSpark/query39.sql
+++ client/project/src/main/resources/QueriesSpark/query39.sql
@@ -15,11 +15,11 @@ with inv as
       group by w_warehouse_name,w_warehouse_sk,i_item_sk,d_moy) foo
  where case mean when 0 then 0 else stdev/mean end > 1)
 select inv1.w_warehouse_sk,inv1.i_item_sk,inv1.d_moy,inv1.mean, inv1.cov
-		,inv2.w_warehouse_sk as w_warehouse_sk2,
-		inv2.i_item_sk as i_item_sk2,
-		inv2.d_moy as d_moy2,
-		inv2.mean as mean2,
-		inv2.cov as cov2
+        ,inv2.w_warehouse_sk as w_warehouse_sk2,
+        inv2.i_item_sk as i_item_sk2,
+        inv2.d_moy as d_moy2,
+        inv2.mean as mean2,
+        inv2.cov as cov2
 from inv inv1,inv inv2
 where inv1.i_item_sk = inv2.i_item_sk
   and inv1.w_warehouse_sk =  inv2.w_warehouse_sk
@@ -44,11 +44,11 @@ with inv as
       group by w_warehouse_name,w_warehouse_sk,i_item_sk,d_moy) foo
  where case mean when 0 then 0 else stdev/mean end > 1)
 select inv1.w_warehouse_sk,inv1.i_item_sk,inv1.d_moy,inv1.mean, inv1.cov
-		,inv2.w_warehouse_sk as w_warehouse_sk2,
-		inv2.i_item_sk as i_item_sk2,
-		inv2.d_moy as d_moy2,
-		inv2.mean as mean2,
-		inv2.cov as cov2
+        ,inv2.w_warehouse_sk as w_warehouse_sk2,
+        inv2.i_item_sk as i_item_sk2,
+        inv2.d_moy as d_moy2,
+        inv2.mean as mean2,
+        inv2.cov as cov2
 from inv inv1,inv inv2
 where inv1.i_item_sk = inv2.i_item_sk
   and inv1.w_warehouse_sk =  inv2.w_warehouse_sk
--- client/project/src/main/resources/QueriesSpark/query80.sql
+++ client/project/src/main/resources/QueriesSpark/query80.sql
@@ -12,7 +12,7 @@ with ssr as
      promotion
  where ss_sold_date_sk = d_date_sk
        and date(d_date) between cast('1998-08-04' as date) 
-                  and (cast('1998-08-04' as date) +  interval '30' day)
+                  and (cast('1998-08-04' as date) + interval '30' day)
        and ss_store_sk = s_store_sk
        and ss_item_sk = i_item_sk
        and i_current_price > 50
@@ -54,7 +54,7 @@ group by cp_catalog_page_id)
      promotion
  where ws_sold_date_sk = d_date_sk
        and date(d_date) between cast('1998-08-04' as date)
-                  and (cast('1998-08-04' as date) +  interval '30' day)
+                  and (cast('1998-08-04' as date) + interval '30' day)
         and ws_web_site_sk = web_site_sk
        and ws_item_sk = i_item_sk
        and i_current_price > 50
