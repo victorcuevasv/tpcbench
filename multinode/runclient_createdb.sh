13993ea2bd7d8520b1572b8b61014d346ae4c443
victorcuevasv <victorcuevasv@gmail.com>
2019-02-19 18:31:04 +0100

    Set right container in the create database script.

---
+++ multinode/runclient_createdb.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Execute the Java project with Maven by running the container (standalone container, no docker-compose). Can fail due to being unable to resolve localhost to the hiveservercontainer.
+
+#docker run --rm -v $(pwd)/project:/project  --entrypoint mvn buildhiveclient:dev exec:java -Dexec.mainClass="org.bsc.dcc.vcv.ProcessCreateScript" -Dexec.args="/data tpcds.sql" -f /project/pom.xml   
+
+#Execute the Java project with Maven on the buildhiveclient container running in docker-compose. 
+
+#docker exec -ti  hiveclientcontainer  /bin/bash -c "mvn exec:java -Dexec.mainClass=\"org.bsc.dcc.vcv.ProcessCreateScript\" -Dexec.args=\"/data tpcds.sql\" -f /project/pom.xml"
+
+docker exec -ti hiveclientcontainer  /bin/bash -c "mvn exec:java -Dexec.mainClass=\"org.bsc.dcc.vcv.CreateDatabase\" -Dexec.args=\"/data/tables _ext /temporal/1GB namenodecontainer\" -f /project/pom.xml" 
+
