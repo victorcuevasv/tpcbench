c6e5a502df48afe1a9462691c6e8bda5361e7b4f
victorcuevasv <victorcuevasv@gmail.com>
2019-02-21 16:31:13 +0100

    Specify apache mirror as dockerfile argument for spark slave.

--- multinode/server/buildSlavesPresto.sh
+++ multinode/server/buildSlavesPresto.sh
@@ -2,5 +2,5 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-docker build -t sparkslave:dev $DIR -f $DIR/DockerfileSlavePresto 
+docker build -t sparkslave:dev $DIR -f $DIR/DockerfileSlavePresto --build-arg APACHE_MIRROR=apache.uvigo.es 
 
