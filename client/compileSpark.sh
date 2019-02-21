4578e0a0775137ffb89bd5658baea5ca6db65078
victorcuevasv <victorcuevasv@gmail.com>
2019-02-21 19:22:12 +0100

    Use run instead of exec to compile the spark app on the container.

--- client/compileSpark.sh
+++ client/compileSpark.sh
@@ -3,9 +3,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 #Build the project with the container using Maven by running the container (standalone, with no docker-compose).
-#docker run --rm -v $DIR/project:/project  \
-#--entrypoint mvn  buildsparkhiveclient:dev clean package -f /project/pomSpark.xml   
+docker run --rm -v $DIR/project:/project  \
+--entrypoint mvn  buildsparkhiveclient:dev clean package -f /project/pomSpark.xml   
 
 #If the sparkhiveclientcontainer container is running in the docker compose.
-docker exec -ti  sparkhiveclientcontainer  /bin/bash -c "mvn clean package -q -f /project/pomSpark.xml"
+#docker exec -ti  sparkhiveclientcontainer  /bin/bash -c "mvn clean package -q -f /project/pomSpark.xml"
 
