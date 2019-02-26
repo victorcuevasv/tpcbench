7d7cad0cd31e29fb9bed70beff1a87f2bb6775b0
victorcuevasv <victorcuevasv@gmail.com>
2019-02-26 19:19:12 +0100

    Loop over the folders to convert .log to .xls.

--- analysis/Documents/ConvertToXLSX.R
+++ analysis/Documents/ConvertToXLSX.R
@@ -1,6 +1,17 @@
 library("rio")
 
-analytics <- import("./Documents/RESULTS/presto_power/analytics.log", format="psv")
+workDir <- "./Documents/RESULTS/power"
+systemDirs <- list.files(path=workDir)
 
-export(analytics, "./Documents/RESULTS/presto_power/analytics.xlsx")
+#It is assumed that there is a single .log file with the run data.
+for(system in systemDirs) {
+  systemDir <- paste(workDir, "/", system, sep="")
+  files <- list.files(path=systemDir, pattern = "\\.log$")
+  inFile <- paste(workDir, "/", system, "/", files[1], sep="")
+  outFileName <- paste(tools::file_path_sans_ext(files[1]), ".xlsx", sep="")
+  outFile <- paste(workDir, "/", system, "/", outFileName, sep="")
+  analytics <- import(inFile, format="psv")
+  export(analytics, outFile)
+}
+
 
