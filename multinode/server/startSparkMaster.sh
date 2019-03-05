0ee70ac13f311f60ff191eeec723ae9a1f7a8382
victorcuevasv <victorcuevasv@gmail.com>
2019-03-05 13:51:11 +0100

    Modify command to run spark thrift server.

--- multinode/server/startSparkMaster.sh
+++ multinode/server/startSparkMaster.sh
@@ -56,7 +56,7 @@ hive --service hiveserver2 &
 wait_for_server localhost 10000 24
 bash /opt/spark-2.4.0-bin-hadoop2.7/sbin/start-all.sh
 bash /opt/spark-2.4.0-bin-hadoop2.7/sbin/start-history-server.sh
-bash /opt/spark-2.4.0-bin-hadoop2.7/sbin/start-thriftserver.sh --hiveconf hive.server2.thrift.port=10015 
+bash /opt/spark-2.4.0-bin-hadoop2.7/sbin/start-thriftserver.sh --master spark://namenodecontainer:7077 --hiveconf hive.server2.thrift.port=10015     
 
 sleep infinity
 
