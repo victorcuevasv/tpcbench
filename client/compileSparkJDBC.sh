0d0fe7461725b9e66414849014a3b1b96839c767
victorcuevasv <victorcuevasv@gmail.com>
2019-03-06 12:26:03 +0100

    Add maven configuration to compile Spark JDBC.

---
+++ client/compileSparkJDBC.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+#Build the project with the container using Maven by running the container (standalone, with no docker-compose).
+docker run --rm -v $DIR/project:/project  \
+--entrypoint mvn  buildsparkhiveclient:dev clean package -f /project/pomSparkJDBC.xml   
+
+#If the sparkhiveclientcontainer container is running in the docker compose.
+#docker exec -ti  sparkhiveclientcontainer  /bin/bash -c "mvn clean package -q -f /project/pomSpark.xml"
+
