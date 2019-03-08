2df42a56cd17391cf504914b91543a4a20738ef7
victorcuevasv <victorcuevasv@gmail.com>
2019-03-08 16:18:24 +0100

    Change concorrent analytics log format.

--- client/project/src/main/java/org/bsc/dcc/vcv/AnalyticsRecorderConcurrent.java
+++ client/project/src/main/java/org/bsc/dcc/vcv/AnalyticsRecorderConcurrent.java
@@ -10,8 +10,8 @@ public class AnalyticsRecorderConcurrent extends AnalyticsRecorder {
 	}
 	
 	public void header() {
-		String[] titles = {"STREAM", "QUERY", "SUCCESSFUL", "STARTDATE_EPOCH", "STOPDATE_EPOCH",
-				                 "DURATION_MS", "STARTDATE", "STOPDATE", "DURATION", "RESULTS_SIZE"};
+		String[] titles = {"STREAM", "QUERY", "SUCCESSFUL", "DURATION", "RESULTS_SIZE", "SYSTEM", 
+				   "STARTDATE_EPOCH", "STOPDATE_EPOCH", "DURATION_MS", "STARTDATE", "STOPDATE"};
 		StringBuilder builder = new StringBuilder();
 		for(String title : titles)
 			builder.append(String.format("%-25s|", title));
@@ -25,19 +25,20 @@ public class AnalyticsRecorderConcurrent extends AnalyticsRecorder {
 		builder.append(String.format(colFormat, queryRecord.getStream()));
 		builder.append(String.format(colFormat, queryRecord.getQuery()));
 		builder.append(String.format(colFormat, queryRecord.isSuccessful()));
+		long durationMs = queryRecord.getEndTime() - queryRecord.getStartTime();
+		String durationFormatted = String.format("%.3f", ((double) durationMs / 1000.0d));
+		builder.append(String.format(colFormat, durationFormatted));
+		builder.append(String.format(colFormat, queryRecord.getResultsSize()));
+		builder.append(String.format(colFormat, this.system));
+		builder.append(String.format(colFormat, durationMs));
 		builder.append(String.format(colFormat, queryRecord.getStartTime()));
 		builder.append(String.format(colFormat, queryRecord.getEndTime()));
-		long durationMs = queryRecord.getEndTime() - queryRecord.getStartTime();
-		builder.append(String.format(colFormat, durationMs));
 		Date startDate = new Date(queryRecord.getStartTime());
 		String startDateFormatted = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(startDate);
 		builder.append(String.format(colFormat, startDateFormatted));
 		Date endDate = new Date(queryRecord.getEndTime());
 		String endDateFormatted = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(endDate);
-		builder.append(String.format(colFormat, endDateFormatted));
-		String durationFormatted = String.format("%.3f", ((double) durationMs / 1000.0d));
-		builder.append(String.format(colFormat, durationFormatted));
-		builder.append(String.format(colFormat, queryRecord.getResultsSize()));
+		builder.append(String.format("%-" + spaces + "s", endDateFormatted));
 		logger.info(builder.toString());
 	}
 
