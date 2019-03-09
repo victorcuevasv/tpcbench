a0ccee029aa9650297a6e0d06a5a08ee0755374b
victorcuevasv <victorcuevasv@gmail.com>
2019-03-09 18:27:15 +0100

    Add script to merge summarized power test excel files.

---
+++ analysis/Documents/MergeSummarizedLogs.R
@@ -0,0 +1,35 @@
+library("rio")
+
+docsDir <- "./Documents"
+outFileName <- "summaryPower.xlsx"
+outFile <- paste(workDir, "/", system, "/", outFileName, sep="")
+workDir <- "./Documents/RESULTS/power"
+systemDirs <- list.files(path=workDir)
+outputDF <- NULL
+firstFile <- TRUE
+
+for(system in systemDirs) {
+  systemDir <- paste(workDir, "/", system, sep="")
+  inFile <- paste(workDir, "/", system, "/", "summary.xlsx", sep="")
+  if( firstFile ) {
+    outputDF <- import(inFile)
+  }
+  else {
+    tempDF <- import(inFile)
+    outputDF[nrow(outputDF) + 1,] = tempDF[1,]
+  }
+  firstFile <- FALSE
+}
+
+print(outputDF)
+
+
+
+
+
+
+
+
+
+
+
