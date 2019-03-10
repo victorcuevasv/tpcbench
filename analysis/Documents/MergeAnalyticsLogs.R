019a73532506c9525ee4ba7a1a0932cd08c8e49a
victorcuevasv <victorcuevasv@gmail.com>
2019-03-10 17:56:07 +0100

    Add script to merge analytics logs.

---
+++ analysis/Documents/MergeAnalyticsLogs.R
@@ -0,0 +1,48 @@
+library("rio")
+
+destDir <- "./Documents/RESULTS/power"
+outFileName <- "mergedPower.xlsx"
+outFile <- paste(destDir, "/", outFileName, sep="")
+workDir <- "./Documents/RESULTS/power"
+searchedFile <- "analytics.xlsx"
+
+#Merge the instances of the searchedFile (must be .xlsx files) found in the
+#workDir, and generate an output xlsx file as specified by outFile. 
+#It is assumed that the instances of the searchedFile are inside each of the
+#subdirectories of the workDir.
+mergeXLSXFiles <- function(workDir, searchedFile, outFile) {
+  subDirs <- list.files(path=workDir)
+  outputDF <- NULL
+  firstFile <- TRUE
+  for(subDir in subDirs) {
+    #Skip files, only consider folders.
+    if( file_test("-f", paste0(workDir, "/", subDir)) )
+      next
+    inFile <- paste(workDir, "/", subDir, "/", searchedFile, sep="")
+    if( firstFile ) {
+      outputDF <- import(inFile)
+    }
+    else {
+      tempDF <- import(inFile)
+      for( row in 1:nrow(tempDF)  )
+        outputDF[nrow(outputDF) + 1,] = tempDF[row,]
+    }
+    firstFile <- FALSE
+  }
+  print(outputDF)
+  export(outputDF, outFile)
+}
+
+mergeXLSXFiles(workDir, searchedFile, outFile)
+
+
+
+
+
+
+
+
+
+
+
+
