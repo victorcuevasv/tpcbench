2d59c3ef4b93ca5470cc9a10acf22b1d955b5dcf
victorcuevasv <victorcuevasv@gmail.com>
2019-03-13 15:18:19 +0100

    Update hive example.

--- client/project/src/main/java/org/bsc/dcc/vcv/HiveJdbcClient.java
+++ client/project/src/main/java/org/bsc/dcc/vcv/HiveJdbcClient.java
@@ -13,6 +13,8 @@ public class HiveJdbcClient {
     /**
      * @param args
      * @throws SQLException
+     * 
+     * [0] server hostname
      */
     public static void main(String[] args) throws SQLException {
         try {
@@ -23,14 +25,19 @@ public class HiveJdbcClient {
             e.printStackTrace();
             System.exit(1);
         }
-        // replace "hive" here with the name of the user the queries should run
-        // as
+        // replace "hive" here with the name of the user the queries should run as
         //Connection con = DriverManager.getConnection("jdbc:hive2://localhost:10000/default", "hive", "");
-        Connection con = DriverManager.getConnection("jdbc:hive2://hiveservercontainer:10000/default", "hive", "");
+        Connection con = DriverManager.getConnection("jdbc:hive2://" + args[0] + 
+        		":10000/default", "hive", "");
         Statement stmt = con.createStatement();
         String tableName = "testHiveDriverTable";
         stmt.execute("drop table if exists " + tableName);
-        stmt.execute("create table " + tableName + " (key int, value string)");
+        stmt.execute("create external table " + tableName + 
+        		" (r_reason_sk int, r_reason_id char(16)," + 
+        		" r_reason_desc char(100) ) ROW FORMAT DELIMITED FIELDS TERMINATED BY '|'" + 
+        		" STORED AS TEXTFILE" +  
+        		" LOCATION '/temporal/1GB/reason'");
+        
         // show tables
         String sql = "show tables '" + tableName + "'";
         System.out.println("Running: " + sql);
@@ -38,28 +45,23 @@ public class HiveJdbcClient {
         if (res.next()) {
             System.out.println(res.getString(1));
         }
+        
         // describe table
         sql = "describe " + tableName;
         System.out.println("Running: " + sql);
         res = stmt.executeQuery(sql);
         while (res.next()) {
-            System.out.println(res.getString(1) + "\t" + res.getString(2));
+            System.out.println(res.getString(1) + "\t" + res.getString(2) + 
+            		"\t" + res.getString(3));
         }
-
-        // load data into table
-        // NOTE: filepath has to be local to the hive server
-        // NOTE: /tmp/a.txt is a ctrl-A separated file with two fields per line
-        String filepath = "/temporal/a.txt";
-        sql = "load data local inpath '" + filepath + "' into table " + tableName;
-        System.out.println("Running: " + sql);
-        stmt.execute(sql);
 
         // select * query
         sql = "select * from " + tableName;
         System.out.println("Running: " + sql);
         res = stmt.executeQuery(sql);
         while (res.next()) {
-            System.out.println(String.valueOf(res.getInt(1)) + "\t" + res.getString(2));
+            System.out.println(String.valueOf(res.getInt(1)) + "\t" + res.getString(2)
+            	+ "\t" + res.getString(3));
         }
 
         // regular hive query
---
+++ runclient_hivetest.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#Execute the Java project with Maven on the client builder container running in docker-compose. 
+
+docker exec -ti  clientbuildercontainer  /bin/bash -c "mvn exec:java -Dexec.mainClass=\"org.bsc.dcc.vcv.HiveJdbcClient\" -Dexec.args=\"mastercontainer\" -f /project/pom.xml"      
+
