0e340637cf3576584c9623b06176186c65a87a4c
victorcuevasv <victorcuevasv@gmail.com>
2019-03-14 01:16:42 +0100

    Tune spark deployment.

--- runclient_executequeriesspark.sh
+++ runclient_executequeriesspark.sh
@@ -19,6 +19,12 @@
 
 #Test query execution with Spark.
 
-docker exec -ti  mastercontainer  /bin/bash -c "/opt/spark-2.4.0-bin-hadoop2.7/bin/spark-submit --packages org.apache.logging.log4j:log4j-api:2.11.1,org.apache.logging.log4j:log4j-core:2.11.1 --class org.bsc.dcc.vcv.ExecuteQueriesSpark --master spark://mastercontainer:7077 --deploy-mode client /temporal/client-1.0-SNAPSHOT.jar /temporal results plans /temporal/client-1.0-SNAPSHOT.jar"               
+docker exec -ti  mastercontainer  /bin/bash -c \
+	"/opt/spark-2.4.0-bin-hadoop2.7/bin/spark-submit \
+	--packages org.apache.logging.log4j:log4j-api:2.8.2,org.apache.logging.log4j:log4j-core:2.8.2 \
+	--class org.bsc.dcc.vcv.ExecuteQueriesSpark \
+	--master spark://mastercontainer:7077 --deploy-mode client \
+	/project/targetspark/client-1.0-SNAPSHOT.jar \
+	/data results plans /project/targetspark/client-1.0-SNAPSHOT.jar"               
 
 #mvn exec:java -Dexec.mainClass="org.bsc.dcc.vcv.JarQueriesReaderAsZipFile" -Dexec.args="client/project/target/client-1.0-SNAPSHOT.jar" -f client/project/pomSpark.xml
