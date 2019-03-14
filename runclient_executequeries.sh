92e0196c6abdc488249cb5ce3a8706ccf6d94ea9
victorcuevasv <victorcuevasv@gmail.com>
2019-03-14 11:42:13 +0100

    Reformat scripts.

--- runclient_executequeries.sh
+++ runclient_executequeries.sh
@@ -3,5 +3,6 @@
 #Execute the Java project with Maven on the client builder container running in the docker-compose setup. 
 
 docker exec -ti  clientbuildercontainer  /bin/bash -c \
-	"mvn exec:java -Dexec.mainClass=\"org.bsc.dcc.vcv.ExecuteQueries\" -Dexec.args=\"/data QueriesPresto results plans presto mastercontainer \" -f /project/pom.xml"      
+	"mvn exec:java -Dexec.mainClass=\"org.bsc.dcc.vcv.ExecuteQueries\" \
+	-Dexec.args=\"/data QueriesPresto results plans presto mastercontainer query3.sql \" -f /project/pom.xml"      
 
