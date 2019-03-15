72ad46c4d057582ab8ef8ddb8dcf9121317222cb
victorcuevasv <victorcuevasv@gmail.com>
2019-03-15 12:07:47 +0100

    Re-enable build scripts after test.

--- buildAll.sh
+++ buildAll.sh
@@ -50,16 +50,16 @@ USER_ID=$(id -u)
 GROUP_ID=$(id -g)
 
 #Build the Ubuntu with Java base image.
-#printf "\n\n%s\n\n" "${mag}Creating the Ubuntu with Java base image.${end}"
-#bash ubuntujava/build.sh
+printf "\n\n%s\n\n" "${mag}Creating the Ubuntu with Java base image.${end}"
+bash ubuntujava/build.sh
 
 #Build the dqgen image with the TPC-DS toolkit to generate data and queries.
-#printf "\n\n%s\n\n" "${mag}Creating the dqgen TPC-DS toolkit image.${end}"
-#bash dqgen/createContainer.sh $USER_NAME $USER_ID $GROUP_ID
+printf "\n\n%s\n\n" "${mag}Creating the dqgen TPC-DS toolkit image.${end}"
+bash dqgen/createContainer.sh $USER_NAME $USER_ID $GROUP_ID
 
 #Build the presto server image.
-#printf "\n\n%s\n\n" "${mag}Building the Presto server image.${end}"
-#bash server/buildPresto.sh
+printf "\n\n%s\n\n" "${mag}Building the Presto server image.${end}"
+bash server/buildPresto.sh
 
 #Build the Spark Master server image.
 printf "\n\n%s\n\n" "${mag}Building the Spark Master server image.${end}"
@@ -68,8 +68,6 @@ bash server/buildSparkMaster.sh $USER_NAME $USER_ID $GROUP_ID
 #Build the Spark Worker server image.
 printf "\n\n%s\n\n" "${mag}Building the Spark Worker server image.${end}"
 bash server/buildSparkWorker.sh
-
-exit 0
 
 #Build the client project builder image.
 printf "\n\n%s\n\n" "${mag}Building the client project builder image.${end}"
