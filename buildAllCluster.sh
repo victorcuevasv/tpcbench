ee4839e6fbb9c53902d2b7613f9c4e5a2d100cf6
victorcuevasv <victorcuevasv@gmail.com>
2019-03-16 16:58:37 +0100

    Update script for cluster deployment.

--- buildAllCluster.sh
+++ buildAllCluster.sh
@@ -10,65 +10,20 @@ mag=$'\e[1;35m'
 cyn=$'\e[1;36m'
 end=$'\e[0m'
 
-#Create the hivevol directory if it does not exist.
-
-if [ ! -d hivevol ]; then
-   mkdir hivevol
-fi
-
-#Create the metastorevol directory if it does not exist.
-
-if [ ! -d metastorevol ]; then
-   mkdir metastorevol
-fi
-   
-#Create the warehousevol directory if it does not exist.
-
-if [ ! -d warehousevol ]; then
-   mkdir warehousevol
-fi
-
-#Create the datavol directory and its subdirectories if it does not exist.
-
-if [ ! -d datavol ]; then
-   mkdir datavol
-   mkdir datavol/tables
-   mkdir datavol/tablestextfile
-   mkdir datavol/tablesparquet
-   mkdir datavol/results
-   mkdir datavol/plans
-   mkdir datavol/logs
-   #Copy the queries file into the datavol directory.
-   cp benchmark/tpcds.sql datavol
-fi
-
-#Build the Ubuntu with Java base image.
-printf "\n\n%s\n\n" "${mag}Creating Ubuntu with Java base image.${end}"
-bash ubuntujava/build.sh
-
-#Build the server image.
-printf "\n\n%s\n\n" "${mag}Building the server image.${end}"
-bash server/buildPresto.sh
-
-#Build the client project builder image.
-printf "\n\n%s\n\n" "${mag}Building the client project builder image.${end}"
-bash client/buildPresto.sh
+#Generate the unused Netezza queries.
+printf "\n\n%s\n\n" "${cyn}Executing the buildAll.sh script.${end}"
+bash buildAll.sh
 
 #Set permissions for data volume and client project.
-chmod -R 777 client/project
-sudo chmod -R 777 client/project
-chmod -R 777 datavol
-sudo chmod -R 777 datavol
-chmod -R 777 warehousevol
-sudo chmod -R 777 warehousevol
 chmod -R 777 metastorevol
-sudo chmod -R 777 metastorevol
-chmod -R 777 hivevol
-sudo chmod -R 777 hivevol
-
-#Compile the client project.
-printf "\n\n%s\n\n" "${blu}Compiling the client project.${end}"
-bash client/compile.sh
-
+#sudo chmod -R 777 metastorevol
+#chmod -R 777 client/project
+#sudo chmod -R 777 client/project
+#chmod -R 777 datavol
+#sudo chmod -R 777 datavol
+#chmod -R 777 warehousevol
+#sudo chmod -R 777 warehousevol
+#chmod -R 777 hivevol
+#sudo chmod -R 777 hivevol
 
 
--- clusterPermissions.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-chmod -R 777 client/project
-sudo chmod -R 777 client/project
-chmod -R 777 datavol
-sudo chmod -R 777 datavol
-
-
-
