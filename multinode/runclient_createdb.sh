4e7b307539db64846e7eb6f74af65e841e7ac7e9
victorcuevasv <victorcuevasv@gmail.com>
2019-03-17 22:31:36 +0100

    Add parameter for the container.

--- multinode/runclient_createdb.sh
+++ multinode/runclient_createdb.sh
@@ -4,6 +4,6 @@
 #If the multinode docker-compose setup is running, the database will be created in this setup.
 
 #Pass the first argument to the script in the parent directory.
-bash ../runclient_createdb.sh $1
+bash ../runclient_createdb.sh $1 namenodecontainer
 
 
