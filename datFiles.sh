8470207efd3f73b491dfde6f17b35992c96dd827
victorcuevasv <victorcuevasv@gmail.com>
2019-03-17 22:14:33 +0100

    Add execution directory path.

--- datFiles.sh
+++ datFiles.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
 #For each .dat file, create a directory with that name and move the .dat file to
 #that directory.
 
-for f in ./hivevol/$1GB/*.dat ; do 
+for f in $DIR/hivevol/$1GB/*.dat ; do 
    fileName=$(basename "$f" .dat) #This invocation removes the .dat extension.
-   mkdir ./hivevol/$1GB/$fileName
-   mv ./hivevol/$1GB/"$fileName".dat ./hivevol/$1GB/$fileName
+   mkdir $DIR/hivevol/$1GB/$fileName
+   mv $DIR/hivevol/$1GB/"$fileName".dat $DIR/hivevol/$1GB/$fileName
 done
 
 
