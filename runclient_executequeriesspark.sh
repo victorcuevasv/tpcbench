09abb492abe02843c014911d58b0f413f2c3ea97
victorcuevasv <victorcuevasv@gmail.com>
2019-03-17 23:59:20 +0100

    Fix paths to jars.

--- runclient_executequeriesspark.sh
+++ runclient_executequeriesspark.sh
@@ -12,6 +12,6 @@ docker exec --user $USER_ID:$GROUP_ID -ti  mastercontainer  /bin/bash -c \
 	--packages org.apache.logging.log4j:log4j-api:2.8.2,org.apache.logging.log4j:log4j-core:2.8.2 \
 	--class org.bsc.dcc.vcv.ExecuteQueriesSpark \
 	--master spark://mastercontainer:7077 --deploy-mode client \
-	/project/client-1.0-SNAPSHOT.jar \
-	/data results plans /project/client-1.0-SNAPSHOT.jar query3.sql"               
+	/project/targetspark/client-1.0-SNAPSHOT.jar \
+	/data results plans /project/targetspark/client-1.0-SNAPSHOT.jar query3.sql"               
 	
