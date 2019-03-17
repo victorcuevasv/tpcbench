cf7bfe971d0b1ffa304bfbe347d4b8699754919d
victorcuevasv <victorcuevasv@gmail.com>
2019-03-17 23:30:11 +0100

    Correct reference to client container.

--- multinode/runclient_executequeriessparkjdbc.sh
+++ multinode/runclient_executequeriessparkjdbc.sh
@@ -2,7 +2,7 @@
 
 #Execute the Java project with Maven on the buildhiveclient container running in docker-compose. 
 
-docker exec -ti  namenodecontainer  /bin/bash -c \
+docker exec -ti  clientbuildercontainer  /bin/bash -c \
 	"mvn exec:java -Dexec.mainClass=\"org.bsc.dcc.vcv.ExecuteQueries\" \
 	-Dexec.args=\"/data QueriesPresto results plans spark namenodecontainer query2.sql\" \
 	-f /project/pomSparkJDBC.xml"
