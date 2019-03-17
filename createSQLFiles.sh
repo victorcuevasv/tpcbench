b44640f61e5a578d12595efdf192969a9a966d4a
victorcuevasv <victorcuevasv@gmail.com>
2019-03-17 21:56:00 +0100

    Add execution directory to script.

--- createSQLFiles.sh
+++ createSQLFiles.sh
@@ -10,6 +10,8 @@ mag=$'\e[1;35m'
 cyn=$'\e[1;36m'
 end=$'\e[0m'
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
 #Get the user id of the user executing this script.
 USER_ID=$(id -u)
 #Get the user id of the user executing this script.
@@ -19,21 +21,21 @@ GROUP_ID=$(id -g)
 
 #First separate the multiple create table statements in the tpcds.sql file into separate files.
 #Copy the file with create table statements into the datavol directory.
-cp dqgen/v2.10.1rc3/tools/tpcds.sql datavol
+cp $DIR/dqgen/v2.10.1rc3/tools/tpcds.sql datavol
 printf "\n\n%s\n\n" "${mag}Processing the tpcds.sql file.${end}"
-bash runclient_processcreatescript.sh $USER_ID $GROUP_ID
+bash $DIR/runclient_processcreatescript.sh $USER_ID $GROUP_ID
 
 #Generate the unused Netezza queries.
 printf "\n\n%s\n\n" "${mag}Generating the Presto queries.${end}"
-bash dqgen/generateQueries.sh $USER_ID $GROUP_ID
+bash $DIR/dqgen/generateQueries.sh $USER_ID $GROUP_ID
 
 #Generate the Presto queries.
 printf "\n\n%s\n\n" "${mag}Generating the Presto queries.${end}"
-bash dqgen/generateQueriesPresto.sh $USER_ID $GROUP_ID
+bash $DIR/dqgen/generateQueriesPresto.sh $USER_ID $GROUP_ID
 
 #Generate the Spark queries.
 printf "\n\n%s\n\n" "${mag}Generating the Spark queries.${end}"
-bash dqgen/generateQueriesSpark.sh $USER_ID $GROUP_ID
-cp -r datavol/QueriesSpark client/project/src/main/resources/
+bash $DIR/dqgen/generateQueriesSpark.sh $USER_ID $GROUP_ID
+cp -r $DIR/datavol/QueriesSpark $DIR/client/project/src/main/resources/
 
 
