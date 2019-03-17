24d61b866bded31ef7f45ac29f6eb1e8e72ade4d
victorcuevasv <victorcuevasv@gmail.com>
2019-03-17 23:52:20 +0100

    Update execution scripts for Spark with spark-submit.

--- multinode/runclient_executequeriesspark.sh
+++ multinode/runclient_executequeriesspark.sh
@@ -8,7 +8,7 @@ docker exec -ti  namenodecontainer  /bin/bash -c \
 	--packages org.apache.logging.log4j:log4j-api:2.8.2,org.apache.logging.log4j:log4j-core:2.8.2 \
 	--class org.bsc.dcc.vcv.ExecuteQueriesSpark \
 	--master spark://namenodecontainer:7077 --deploy-mode client \
-	/project/targetspark/client-1.0-SNAPSHOT.jar \
-	/data results plans /project/targetspark/client-1.0-SNAPSHOT.jar query2.sql"               
+	/project/client-1.0-SNAPSHOT.jar \
+	/data results plans /project/client-1.0-SNAPSHOT.jar query3.sql"               
 
 
--- runclient_executequeriesconcurrentspark.sh
+++ runclient_executequeriesconcurrentspark.sh
@@ -20,6 +20,6 @@ docker exec --user $USER_ID:$GROUP_ID -ti  mastercontainer  /bin/bash -c \
 	--packages org.apache.logging.log4j:log4j-api:2.8.2,org.apache.logging.log4j:log4j-core:2.8.2 \
 	--class org.bsc.dcc.vcv.ExecuteQueriesConcurrentSpark \
 	--master spark://mastercontainer:7077 --deploy-mode client \
-	/project/targetspark/client-1.0-SNAPSHOT.jar \
-	/data results plans /project/targetspark/client-1.0-SNAPSHOT.jar 2 1954"                      
+	/project/client-1.0-SNAPSHOT.jar \
+	/data results plans /project/client-1.0-SNAPSHOT.jar 2 1954"                      
 
