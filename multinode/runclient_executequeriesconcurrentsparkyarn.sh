ff17222a6f4bc2db085e28860a22ca9a0719d9a7
victorcuevasv <victorcuevasv@gmail.com>
2019-03-18 17:16:28 +0100

    Add scripts for yarn.

--- multinode/runclient_executequeriesconcurrentsparkyarn.sh
+++ multinode/runclient_executequeriesconcurrentsparkyarn.sh
@@ -5,7 +5,9 @@
 docker exec -ti  namenodecontainer  /bin/bash -c \
 	"/opt/spark-2.4.0-bin-hadoop2.7/bin/spark-submit --conf spark.eventLog.enabled=true  \
 	--packages org.apache.logging.log4j:log4j-api:2.8.2,org.apache.logging.log4j:log4j-core:2.8.2 \
-	--class org.bsc.dcc.vcv.ExecuteQueriesSpark --master yarn --deploy-mode client \
+	--class org.bsc.dcc.vcv.ExecuteQueriesConcurrentSpark \
+	--master yarn --deploy-mode client \
 	/project/targetspark/client-1.0-SNAPSHOT.jar \
-	/data results plans /project/targetspark/client-1.0-SNAPSHOT.jar query2.sql"               
+	/data results plans /project/targetspark/client-1.0-SNAPSHOT.jar 2 1954"                      
+
 
