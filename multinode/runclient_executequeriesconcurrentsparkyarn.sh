d5aa41f3ef516a1d00f27ab59392ee41368737d2
victorcuevasv <victorcuevasv@gmail.com>
2019-03-18 17:50:05 +0100

    Add number of executor instances.

--- multinode/runclient_executequeriesconcurrentsparkyarn.sh
+++ multinode/runclient_executequeriesconcurrentsparkyarn.sh
@@ -5,7 +5,6 @@
 docker exec -ti  namenodecontainer  /bin/bash -c \
 	"/opt/spark-2.4.0-bin-hadoop2.7/bin/spark-submit --conf spark.eventLog.enabled=true  \
 	--packages org.apache.logging.log4j:log4j-api:2.8.2,org.apache.logging.log4j:log4j-core:2.8.2 \
-	--num-executors 22 \
 	--class org.bsc.dcc.vcv.ExecuteQueriesConcurrentSpark \
 	--master yarn --deploy-mode client \
 	/project/targetspark/client-1.0-SNAPSHOT.jar \
