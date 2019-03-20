433f96d2b5422c4b4c4194dc8659f4c02faae295
victorcuevasv <victorcuevasv@gmail.com>
2019-03-20 23:59:07 +0100

    Add events directory.

--- multinode/runclient_executequeriesconcurrentspark.sh
+++ multinode/runclient_executequeriesconcurrentspark.sh
@@ -14,6 +14,7 @@ docker exec --user $USER_ID:$GROUP_ID -ti  namenodecontainer  /bin/bash -c \
 --packages org.apache.logging.log4j:log4j-api:2.8.2,org.apache.logging.log4j:log4j-core:2.8.2,\
 org.apache.zookeeper:zookeeper:3.4.6 \
 --conf spark.local.dir=/home/$USER_NAME/tmp \
+--conf spark.eventLog.dir=/home/$USER_NAME/tmp/spark-events \
 --class org.bsc.dcc.vcv.ExecuteQueriesConcurrentSpark \
 --master spark://namenodecontainer:7077 --deploy-mode client \
 /project/targetspark/client-1.0-SNAPSHOT.jar \
