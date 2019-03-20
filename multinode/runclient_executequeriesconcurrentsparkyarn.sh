a54f68a31e1de3581b7d12cb9d786d4844c8aa16
victorcuevasv <victorcuevasv@gmail.com>
2019-03-21 00:48:41 +0100

    Add zookeeper dependency.

--- multinode/runclient_executequeriesconcurrentsparkyarn.sh
+++ multinode/runclient_executequeriesconcurrentsparkyarn.sh
@@ -3,11 +3,12 @@
 #Test query execution with Spark.
 
 docker exec -ti  namenodecontainer  /bin/bash -c \
-	"/opt/spark-2.4.0-bin-hadoop2.7/bin/spark-submit --conf spark.eventLog.enabled=true  \
-	--packages org.apache.logging.log4j:log4j-api:2.8.2,org.apache.logging.log4j:log4j-core:2.8.2 \
-	--class org.bsc.dcc.vcv.ExecuteQueriesConcurrentSpark \
-	--master yarn --deploy-mode client \
-	/project/targetspark/client-1.0-SNAPSHOT.jar \
-	/data results plans /project/targetspark/client-1.0-SNAPSHOT.jar sparkyarn 2 1954"                      
+"/opt/spark-2.4.0-bin-hadoop2.7/bin/spark-submit --conf spark.eventLog.enabled=true  \
+--packages org.apache.logging.log4j:log4j-api:2.8.2,org.apache.logging.log4j:log4j-core:2.8.2,\
+org.apache.zookeeper:zookeeper:3.4.6 \
+--class org.bsc.dcc.vcv.ExecuteQueriesConcurrentSpark \
+--master yarn --deploy-mode client \
+/project/targetspark/client-1.0-SNAPSHOT.jar \
+/data results plans /project/targetspark/client-1.0-SNAPSHOT.jar sparkyarn 2 1954"                      
 
 
