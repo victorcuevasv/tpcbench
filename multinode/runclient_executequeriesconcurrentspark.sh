3bb4f7d716ee0938c5386845e2792cfc2d6fdf35
victorcuevasv <victorcuevasv@gmail.com>
2019-03-20 17:40:24 +0100

    Fix incorrect specification of packages parameter.

--- multinode/runclient_executequeriesconcurrentspark.sh
+++ multinode/runclient_executequeriesconcurrentspark.sh
@@ -9,8 +9,8 @@ GROUP_ID=$(id -g)
 
 docker exec --user $USER_ID:$GROUP_ID -ti  namenodecontainer  /bin/bash -c \
 	"/opt/spark-2.4.0-bin-hadoop2.7/bin/spark-submit --conf spark.eventLog.enabled=true  \
-	--packages org.apache.logging.log4j:log4j-api:2.8.2,org.apache.logging.log4j:log4j-core:2.8.2 \
-	--packages org.apache.zookeeper:zookeeper:3.4.6 \
+	--packages org.apache.logging.log4j:log4j-api:2.8.2,\
+	org.apache.logging.log4j:log4j-core:2.8.2,org.apache.zookeeper:zookeeper:3.4.6 \
 	--class org.bsc.dcc.vcv.ExecuteQueriesConcurrentSpark \
 	--master spark://namenodecontainer:7077 --deploy-mode client \
 	/project/targetspark/client-1.0-SNAPSHOT.jar \
