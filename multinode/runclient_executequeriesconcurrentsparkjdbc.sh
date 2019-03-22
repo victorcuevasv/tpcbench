128b871c8c48f90592083c9ab41f0e985e025a88
victorcuevasv <victorcuevasv@gmail.com>
2019-03-22 16:22:24 +0100

    Fixed error, parameters not used.

--- multinode/runclient_executequeriesconcurrentsparkjdbc.sh
+++ multinode/runclient_executequeriesconcurrentsparkjdbc.sh
@@ -11,6 +11,6 @@ fi
 
 docker exec -ti  clientbuildercontainer  /bin/bash -c \
 	"mvn exec:java -Dexec.mainClass=\"org.bsc.dcc.vcv.ExecuteQueriesConcurrent\" \
-	-Dexec.args=\"/data QueriesSpark results plans sparkjdbc namenodecontainer 2 1954 true\" \
+	-Dexec.args=\"/data QueriesSpark results plans sparkjdbc namenodecontainer $1 1954 true\" \
 	-f /project/pomSparkJDBC.xml"
 
