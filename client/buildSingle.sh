55ffa49b132197fcfedd777519889d956a06d635
victorcuevasv <victorcuevasv@gmail.com>
2019-03-22 20:50:30 +0100

    Add exit codes.

--- client/buildSingle.sh
+++ client/buildSingle.sh
@@ -6,8 +6,21 @@
 #$2 user id
 #$3 group id
 
+exitCode=0
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 docker build --build-arg UNAME=$1 --build-arg UID=$2 --build-arg GID=$3 \
 	-t clientbuilder:dev $DIR -f $DIR/DockerfileSingle
+
+if [[ $? -ne 0 ]]; then
+	exitCode=1
+fi
+
+if [[ $exitCode -ne 0 ]]; then
+	exit 1
+else
+	exit 0
+fi
+
 
