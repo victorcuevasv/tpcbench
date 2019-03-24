4a4c50a1635ef2199be827caa3e5fda90ae73052
victorcuevasv <victorcuevasv@gmail.com>
2019-03-24 15:07:33 +0100

    Add mirrors to build script.

--- server/buildSparkWorker.sh
+++ server/buildSparkWorker.sh
@@ -4,7 +4,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 exitCode=0
 
-docker build -t sparkworker:dev $DIR -f $DIR/DockerfileSparkWorker
+docker build --network="host" -t sparkworker:dev $DIR -f $DIR/DockerfileSparkWorker \
+	--build-arg APACHE_MIRROR=localhost:8888 \
+	--build-arg POSTGRES_DRIVER_MIRROR=localhost:443 \
+	--build-arg PRESTO_MIRROR=localhost:443
 
 if [[ $? -ne 0 ]]; then
 	exitCode=1;
