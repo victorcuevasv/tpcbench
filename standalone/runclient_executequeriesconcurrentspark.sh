1dca425f932aa38b90bde20d45bcc8a021f775c0
victorcuevasv <victorcuevasv@gmail.com>
2019-03-26 17:58:15 +0100

    Incorporate concurrent execution for spark standalone.

---
+++ standalone/runclient_executequeriesconcurrentspark.sh
@@ -0,0 +1,28 @@
+#!/bin/bash      
+
+#Get the user name of the user executing this script.
+USER_NAME=$(whoami)
+
+if [ $# -lt 1 ]; then
+    echo "Usage bash runclient_executequeriesconcurrentspark.sh <number of streams>."
+    exit 0
+fi
+
+#Run as a subshell inside the USER_NAME home directory because the metastore is stored there.
+#The command is executed in docker as the default root user because otherwise some directories are not created.
+#Note that the master used is local.
+
+docker exec -ti  namenodecontainer  /bin/bash -c \
+"( cd /home/$USER_NAME ; /opt/spark-2.4.0-bin-hadoop2.7/bin/spark-submit --conf spark.eventLog.enabled=true  \
+--packages org.apache.logging.log4j:log4j-api:2.8.2,org.apache.logging.log4j:log4j-core:2.8.2,\
+org.apache.zookeeper:zookeeper:3.4.6 \
+--conf spark.local.dir=/home/$USER_NAME/tmp \
+--conf spark.eventLog.dir=/home/$USER_NAME/tmp \
+--class org.bsc.dcc.vcv.ExecuteQueriesConcurrentSpark \
+--master local \
+/project/targetspark/client-1.0-SNAPSHOT.jar \
+/data results plans /project/targetspark/client-1.0-SNAPSHOT.jar spark $1 1954 )" 
+
+
+                     
+
