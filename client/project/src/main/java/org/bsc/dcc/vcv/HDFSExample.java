c967613744b4dcaf9f4d265053bf53979591ac65
victorcuevasv <victorcuevasv@gmail.com>
2019-03-31 00:02:45 +0100

    Debugging reading from hdfs.

--- client/project/src/main/java/org/bsc/dcc/vcv/HDFSExample.java
+++ client/project/src/main/java/org/bsc/dcc/vcv/HDFSExample.java
@@ -13,14 +13,14 @@ public class HDFSExample {
 	public static void main(String[] args) throws Exception {
 		// File to read in HDFS
 		String uri = args[0];
-		String file = args[1];
 		Configuration conf = new Configuration();
+		System.out.println("Configuration: \n" + conf.toString());
 		// Get the filesystem - HDFS
 		FileSystem fs = FileSystem.get(URI.create(uri), conf);
 		FSDataInputStream in = null;
 		try {
 			// Open the path mentioned in HDFS
-			in = fs.open(new Path(file));
+			in = fs.open(new Path(uri));
 			IOUtils.copyBytes(in, System.out, 4096, false);
 			System.out.println("End Of file: HDFS file read complete");
 		}
