94ae61e53c9c8d2f5aa45e26c0eb883fbea0ed75
victorcuevasv <victorcuevasv@gmail.com>
2019-03-31 00:23:27 +0100

    Debugging reading from hdfs.

--- client/project/src/main/java/org/bsc/dcc/vcv/HDFSExample.java
+++ client/project/src/main/java/org/bsc/dcc/vcv/HDFSExample.java
@@ -14,6 +14,14 @@ public class HDFSExample {
 		// File to read in HDFS
 		String uri = args[0];
 		Configuration conf = new Configuration();
+		// Set FileSystem URI
+		conf.set("fs.defaultFS", uri);
+		// Because of Maven
+		conf.set("fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
+		conf.set("fs.file.impl", org.apache.hadoop.fs.LocalFileSystem.class.getName());
+		// Set HADOOP user
+		System.setProperty("HADOOP_USER_NAME", "hdfs");
+		System.setProperty("hadoop.home.dir", "/");
 		System.out.println("Configuration: \n" + conf.toString());
 		// Get the filesystem - HDFS
 		FileSystem fs = FileSystem.get(URI.create(uri), conf);
