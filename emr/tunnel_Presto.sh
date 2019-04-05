c38d2cbcf2ec30d09369b8a53970f68661ccd444
victorcuevasv <victorcuevasv@gmail.com>
2019-04-06 00:50:08 +0200

    Add script to tunnel presto ports.

---
+++ emr/tunnel_Presto.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#Variables for console output with colors.
+
+red=$'\e[1;31m'
+grn=$'\e[1;32m'
+yel=$'\e[1;33m'
+blu=$'\e[1;34m'
+mag=$'\e[1;35m'
+cyn=$'\e[1;36m'
+end=$'\e[0m'
+
+#Parameters.
+#
+#$1 master node hostname
+
+#Tunnel through ssh the presto web interfaces.
+printf "\n\n%s\n\n" "${cyn}Tunneling the ports for Presto (8088, 19888, 8080).${end}"
+
+ssh -i id_rsa -l hadoop -N -L 8889:localhost:8889 $1
+
+#The trap will kill the background processed started above when the script
+#execution is terminated (ctrl+c).
+trap 'jobs -p | xargs kill' EXIT
+
+#Necessary to avoid immediate script termination.
+sleep infinity
