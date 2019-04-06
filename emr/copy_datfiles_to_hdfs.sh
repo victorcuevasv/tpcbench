3596d90aa6b777a7a5d3ff3993bb7c61b8004cfc
victorcuevasv <victorcuevasv@gmail.com>
2019-04-06 19:05:15 +0200

    Enable the script to run from another directory.

--- emr/copy_datfiles_to_hdfs.sh
+++ emr/copy_datfiles_to_hdfs.sh
@@ -8,6 +8,8 @@ mag=$'\e[1;35m'
 cyn=$'\e[1;36m'
 end=$'\e[0m'
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
 #$1 scale factor (positive integer)
 
 if [ $# -lt 1 ]; then
@@ -18,5 +20,5 @@ fi
 #Copying the .dat files to hdfs.
 printf "\n\n%s\n\n" "${blu}Copying to hdfs.${end}"
 hadoop fs -mkdir -p /temporal/$1GB && \
-	hadoop fs -put ../vols/hive/$1GB/* /temporal/$1GB
+	hadoop fs -put $DIR/../vols/hive/$1GB/* /temporal/$1GB
 
