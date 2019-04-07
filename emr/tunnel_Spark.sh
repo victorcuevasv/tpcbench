9ef041e4feb18fde9516ddde80c2be86527b4359
victorcuevasv <victorcuevasv@gmail.com>
2019-04-07 14:28:49 +0200

    Parametrize the script.

--- emr/tunnel_Spark.sh
+++ emr/tunnel_Spark.sh
@@ -10,20 +10,33 @@ mag=$'\e[1;35m'
 cyn=$'\e[1;36m'
 end=$'\e[0m'
 
-#Generate the unused Netezza queries.
-printf "\n\n%s\n\n" "${cyn}Tunneling the ports for Spark (4040, 4041, 8080, 8088, 18080).${end}"
+#PARAMETERS.
+#
+#$1 Master node Public DNS name.
+#$2 Master node Private DNS name.
 
-ssh -i id_rsa -l ec2-user -N -L 18080:localhost:18080 ec2-18-237-94-60.us-west-2.compute.amazonaws.com &
-ssh -i id_rsa -l ec2-user -N -L 20888:ip-172-31-28-132.us-west-2.compute.internal:20888 \
-	ec2-18-237-94-60.us-west-2.compute.amazonaws.com  &
-ssh -i id_rsa -l ec2-user -N -L 19888:ip-172-31-28-132.us-west-2.compute.internal:19888 \
-	ec2-18-237-94-60.us-west-2.compute.amazonaws.com  &
+if [ $# -lt 2 ]; then
+    echo "Usage bash tunnel_Spark.sh <Public DNS name> <Private DNS name>."
+    exit 0
+fi
 
+#Tunnel the ports for Spark web GUIs.
+printf "\n\n%s\n\n" "${cyn}Tunneling the ports for Spark (18080, 19888, 20888).${end}"
+
+ssh -i id_rsa -l ec2-user -N -L 18080:localhost:18080 $1 &
+ssh -i id_rsa -l ec2-user -N -L 20888:$2:20888 $1  &
+ssh -i id_rsa -l ec2-user -N -L 19888:$2:19888 $1  &
+	
 #History server web gui url.
 #http://localhost:18080/
+	
+#Example command with explicit parameter values.
+#ssh -i id_rsa -l ec2-user -N -L 18080:localhost:18080 ec2-18-237-94-60.us-west-2.compute.amazonaws.com &
+#ssh -i id_rsa -l ec2-user -N -L 20888:ip-172-31-28-132.us-west-2.compute.internal:20888 \
+#	ec2-18-237-94-60.us-west-2.compute.amazonaws.com  &
+#ssh -i id_rsa -l ec2-user -N -L 19888:ip-172-31-28-132.us-west-2.compute.internal:19888 \
+#	ec2-18-237-94-60.us-west-2.compute.amazonaws.com  &
 
-#Example spark application web gui url under the proxy.
-#http://localhost:20888/proxy/application_1554559921361_0012
 
 #The trap will kill the background processed started above when the script
 #execution is terminated (ctrl+c).
@@ -31,4 +44,5 @@ trap 'jobs -p | xargs kill' EXIT
 
 #Necessary to avoid immediate script termination.
 sleep infinity
+
 
