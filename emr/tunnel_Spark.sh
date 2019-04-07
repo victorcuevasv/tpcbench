66277343424347b1a112c38dc2e76380b4222220
victorcuevasv <victorcuevasv@gmail.com>
2019-04-07 02:48:01 +0200

    Fix spark tunneling.

--- emr/tunnel_Spark.sh
+++ emr/tunnel_Spark.sh
@@ -13,8 +13,19 @@ end=$'\e[0m'
 #Generate the unused Netezza queries.
 printf "\n\n%s\n\n" "${cyn}Tunneling the ports for Spark (4040, 4041, 8080, 8088, 18080).${end}"
 
-ssh -i id_rsa -l ec2-user -N -L 4040:localhost:4040 ec2-18-237-94-60.us-west-2.compute.amazonaws.com &
 ssh -i id_rsa -l ec2-user -N -L 18080:localhost:18080 ec2-18-237-94-60.us-west-2.compute.amazonaws.com &
+ssh -i id_rsa -l ec2-user -N -L 20888:ip-172-31-28-132.us-west-2.compute.internal:20888 \
+	ec2-18-237-94-60.us-west-2.compute.amazonaws.com  &               
+ssh -i id_rsa -l ec2-user -N -L 4040:ip-172-31-28-132.us-west-2.compute.internal:4040 \
+	ec2-18-237-94-60.us-west-2.compute.amazonaws.com  &
+
+
+#History server web gui url.
+#http://localhost:18080/
+#Application web gui url in which to find he proxy link.
+#http://localhost:4040
+#Example spark application web gui url under the proxy.
+#http://localhost:20888/proxy/application_1554559921361_0012
 
 #The trap will kill the background processed started above when the script
 #execution is terminated (ctrl+c).
