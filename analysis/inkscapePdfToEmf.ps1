840d26abfc39921887dcf2b81852557f079565fa
victorcuevasv <victorcuevasv@gmail.com>
2019-04-08 00:56:40 +0200

    Add script to convert from pdf to emf in inkscape.

---
+++ analysis/inkscapePdfToEmf.ps1
@@ -0,0 +1,30 @@
+
+$env:Path += ";C:\Program Files\Inkscape"
+
+$DATA_DIR="."
+
+echo ''
+echo "PROCESSING FILES IN DIRECTORY $DATA_DIR"
+echo ''
+
+#Load the data into the tables by psql instructions
+$files = Get-ChildItem $DATA_DIR
+    for ($i=0; $i -lt $files.Count; $i++) {
+		$extn = [IO.Path]::GetExtension($files[$i])
+		if ($extn -eq ".pdf" )
+		{
+			#In PowerShell the extension is removed from the BaseName by default
+			$baseName = $files[$i].BaseName 	
+			$stmtToInvoke = 'inkscape -f=".\$baseName.pdf" --export-emf=".\$baseName.emf"'
+			$stmtToPrint = "inkscape -f=.\$baseName.pdf --export-emf=.\$baseName.emf"			
+			echo $stmtToPrint
+			Invoke-Expression $stmtToInvoke
+		}
+	}
+
+echo ''
+echo 'DONE' 
+echo ''
+
+
+
