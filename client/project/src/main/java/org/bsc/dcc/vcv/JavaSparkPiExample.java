f843f0946bcfa2388be91228e4c5670c8d6b0064
victorcuevasv <victorcuevasv@gmail.com>
2019-04-08 16:11:32 +0200

    Update java spark example.

--- client/project/src/main/java/org/bsc/dcc/vcv/JavaSparkPiExample.java
+++ client/project/src/main/java/org/bsc/dcc/vcv/JavaSparkPiExample.java
@@ -4,6 +4,11 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.SparkSession;
 
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.SaveMode;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -39,6 +44,7 @@ public class JavaSparkPiExample {
 	}
 	
 	private void calculatePi(String namenodeHostname, int slices) {
+		try {
 		JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
 		int n = 100000 * slices;
 		List<Integer> l = new ArrayList<>(n);
@@ -54,12 +60,28 @@ public class JavaSparkPiExample {
 		String results = "Pi is roughly " + 4.0 * count / n;
 		System.out.println(results);
 		this.saveResults(results, System.getenv("HOME") + "/pi.txt");
-		this.logger.info("Copying file to hdfs: " + "hdfs:// " + namenodeHostname + 
-				System.getenv("HOME") + "/pi.txt");
-		HdfsUtil hdfsUtil = new HdfsUtil();
-		hdfsUtil.copyToHdfs(System.getenv("HOME") + "/pi.txt", 
-				"hdfs://" + namenodeHostname + System.getenv("HOME") + "/pi.txt");
+		List<String> resultsList = new ArrayList<String>();
+		resultsList.add(results);
+		Dataset<Row> df = this.spark.createDataset(resultsList, Encoders.STRING()).toDF();
+		this.logger.info("Saving results from the dataframe to HDFS in: " + 
+				System.getenv("HOME") + "/pi_dataframe.txt" );
+		df.write().mode(SaveMode.Overwrite).text(System.getenv("HOME") + "/pi_dataframe.txt");
+		this.logger.info("Saving results from the dataframe to local filesystem in: " + 
+				System.getenv("HOME") + "/pi_dataf.txt" );
+		this.saveDataset(System.getenv("HOME") + "/pi_dataf.txt", df, true);
+//		this.logger.info("Copying file to hdfs: " + "hdfs:// " + namenodeHostname + 
+//				System.getenv("HOME") + "/pi.txt");
+//		HdfsUtil hdfsUtil = new HdfsUtil();
+//		hdfsUtil.copyToHdfs(System.getenv("HOME") + "/pi.txt", 
+//				"hdfs://" + namenodeHostname + System.getenv("HOME") + "/pi.txt");
 		this.spark.stop();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			this.logger.error("Error in calculatePi");
+			this.logger.error(e);
+			this.logger.error(AppUtil.stringifyStackTrace(e));
+		}
 	}
 	
 	private void saveResults(String results, String resFileName) {
@@ -75,6 +97,26 @@ public class JavaSparkPiExample {
 		catch (IOException ioe) {
 			ioe.printStackTrace();
 			this.logger.error(ioe);
+		}
+	}
+	
+	private void saveDataset(String resFileName, Dataset<Row> dataset, boolean append) {
+		try {
+			File tmp = new File(resFileName);
+			tmp.getParentFile().mkdirs();
+			FileWriter fileWriter = new FileWriter(resFileName, append);
+			PrintWriter printWriter = new PrintWriter(fileWriter);
+			List<String> list = dataset.as(Encoders.STRING()).collectAsList();
+			//List<String> list = dataset.map(row -> row.mkString(" | "), Encoders.STRING()).collectAsList();
+			for(String s: list)
+				printWriter.println(s);
+			printWriter.close();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			this.logger.error("Error saving results: " + resFileName);
+			this.logger.error(e);
+			this.logger.error(AppUtil.stringifyStackTrace(e));
 		}
 	}
 
