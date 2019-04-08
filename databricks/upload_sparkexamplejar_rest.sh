8b935d0f6a22880f9e8af6d35f9cad73a359c01b
victorcuevasv <victorcuevasv@gmail.com>
2019-04-08 18:06:31 +0200

    Add script to upload the jar through REST.

---
+++ databricks/upload_sparkexamplejar_rest.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+red=$'\e[1;31m'
+grn=$'\e[1;32m'
+yel=$'\e[1;33m'
+blu=$'\e[1;34m'
+mag=$'\e[1;35m'
+cyn=$'\e[1;36m'
+end=$'\e[0m'
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+#Copying the file to the databricks instance.
+printf "\n\n%s\n\n" "${blu}Uploading the jar using REST API.${end}"
+
+curl -n \
+-F filedata=@"$DIR/../client/project/targetsparkexample/client-1.0-SNAPSHOT-jar-with-dependencies.jar" \
+-F path="/docs/modsparkpi.jar" \
+-F overwrite=true \
+https://dbc-08fc9045-faef.cloud.databricks.com/api/2.0/dbfs/put
+
+
