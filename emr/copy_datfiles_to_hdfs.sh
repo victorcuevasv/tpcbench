21e5a1bfc7ab18673ad794743be9ff49182bee76
victorcuevasv <victorcuevasv@gmail.com>
2019-04-18 12:24:25 +0200

    Add references to S3 files.

--- emr/copy_datfiles_to_hdfs.sh
+++ emr/copy_datfiles_to_hdfs.sh
@@ -13,7 +13,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 #$1 scale factor (positive integer)
 
 if [ $# -lt 1 ]; then
-    echo "${yel}Usage: bash copy_datfiles_to_hdfs.sh <scale factor>.${end}"
+    echo "${yel}Usage: bash copy_datfiles_to_hdfs.sh <scale factor>${end}"
     exit 0
 fi
 
