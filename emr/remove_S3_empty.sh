f6d7dc79f56b482dc9ae6015c14516c5ffc82919
victorcuevasv <victorcuevasv@gmail.com>
2019-05-07 16:11:02 +0200

    Add script to remove empty directories.

---
+++ emr/remove_S3_empty.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+red=$'\e[1;31m'
+grn=$'\e[1;32m'
+yel=$'\e[1;33m'
+blu=$'\e[1;34m'
+mag=$'\e[1;35m'
+cyn=$'\e[1;36m'
+end=$'\e[0m'
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+#$1 file with the table names
+
+if [ $# -lt 2 ]; then
+    echo "${yel}Usage: bash remove_S3_empty.sh <table names file> <S3 bucket name>${end}"
+    echo "${yel}Example: bash remove_S3_empty.sh tableNames.txt tpcds-warehouse-emr-presto-1gb${end}"
+    exit 0
+fi
+
+#Read the file with the table names
+
+#IFS=Internal Field Separator, used by the shell to determine how to do word splitting.
+#Set it to empty to avoid leading/trailing whitespace trimming.
+#For read, use the -r option so that backslash does not act as an escape character.
+#The second conditional prevents the last line from being ignored if it doesn't end with a newline character.
+#The -n option guarantees that the string has a length of more than zero.
+IFS=''
+while read -r line || [[ -n "$line" ]]; do
+	IFS=' '
+	for word in $line; do
+		aws s3api delete-object --bucket $2 --key $word/empty 
+	done
+	IFS=''
+done < "$1"
+
+
+
