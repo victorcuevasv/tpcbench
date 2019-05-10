8f05401c0ae5693bdea21bac7172be025f66d5c4
victorcuevasv <victorcuevasv@gmail.com>
2019-05-11 01:48:10 +0200

    Add cross-join config parameter to stream sessions.

--- client/project/src/main/java/org/bsc/dcc/vcv/QueryStreamSpark.java
+++ client/project/src/main/java/org/bsc/dcc/vcv/QueryStreamSpark.java
@@ -93,6 +93,7 @@ public class QueryStreamSpark implements Callable<Void> {
 	public Void call() {
 		this.logger.info("\n\n\n\n\nStarting query stream: " + this.nStream + " using " + this.dbName);
 		this.useDatabase(this.dbName);
+		this.spark.conf().set("spark.sql.crossJoin.enabled", "true");
 		//this.logger.info(AppUtil.stringifySparkConfiguration(this.spark));
 		//Integer[] queries = this.queriesHT.keySet().toArray(new Integer[] {});
 		//Arrays.sort(queries);
