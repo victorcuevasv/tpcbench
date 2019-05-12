5b3614a053d5f0c702ac52c38791c55b421d45e2
victorcuevasv <victorcuevasv@gmail.com>
2019-05-12 18:26:53 +0200

    Add geometric mean.

--- analysis/Documents/SummarizePowerLog.R
+++ analysis/Documents/SummarizePowerLog.R
@@ -15,9 +15,11 @@ summarizeLog <- function(inFile, outFile, scaleFactor, testDone) {
   analytics$STARTDATE_EPOCH <- as.numeric(analytics$STARTDATE_EPOCH)
   analytics$STOPDATE_EPOCH <- as.numeric(analytics$STOPDATE_EPOCH)
   analytics$DURATION_MS <- as.numeric(analytics$DURATION_MS)
+  analytics$DURATION <- as.numeric(analytics$DURATION)
   totalDurationMSEC <- max(analytics$STOPDATE_EPOCH) - min(analytics$STARTDATE_EPOCH)
   totalDurationSEC <- totalDurationMSEC / 1000
   avgDurationSEC = mean(analytics$DURATION_MS) / 1000
+  geoMeanDurationSEC = prod(analytics$DURATION)^(1/length(analytics$DURATION))
   #Get the system from the first row
   loggedSystem <- analytics$SYSTEM[1]
   #Create a new dataframe to hold the aggregate results.
@@ -26,8 +28,9 @@ summarizeLog <- function(inFile, outFile, scaleFactor, testDone) {
                          SCALE_FACTOR=integer(),
                          TOTAL_DURATION=double(),
                          AVG_QUERY_DURATION=double(),
+                         GEOM_MEAN_DURATION=double(),
                          stringsAsFactors=FALSE)
-  outputDF[nrow(outputDF) + 1,] = list(loggedSystem, testDone, scaleFactor, totalDurationSEC, avgDurationSEC)
+  outputDF[nrow(outputDF) + 1,] = list(loggedSystem, testDone, scaleFactor, totalDurationSEC, avgDurationSEC, geoMeanDurationSEC)
   export(outputDF, outFile)
 }
 
