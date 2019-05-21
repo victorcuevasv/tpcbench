184e6bd950ab632899694b2049570d06ab0639a6
victorcuevasv <victorcuevasv@gmail.com>
2019-05-21 15:58:23 +0200

    Modify script to run without docker.

--- databricks/runclient_executequeries_jdbc.sh
+++ databricks/runclient_executequeries_jdbc.sh
@@ -12,21 +12,12 @@ end=$'\e[0m'
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-#Get the user id of the user executing this script.
-USER_ID=$(id -u)
-#Get the user id of the user executing this script.
-GROUP_ID=$(id -g)
-
 if [ $# -lt 2 ]; then
     echo "${yel}Usage: bash runclient_executequeries_jdbc.sh <scale factor> <all | query filename>${end}"
     exit 0
 fi
 
-docker run --network="host" --rm --user $USER_ID:$GROUP_ID --name clientbuildercontainer -ti \
---volume $DIR/../vols/data:/data \
---volume $DIR/../client/project:/project \
---entrypoint mvn clientbuilder:dev \
-	exec:java -Dexec.mainClass="org.bsc.dcc.vcv.ExecuteQueries" \
+mvn exec:java -Dexec.mainClass="org.bsc.dcc.vcv.ExecuteQueries" \
 	-Dexec.args="/data QueriesSpark results plans sparkdatabricksjdbc $(hostname) true true tpcdsdb$1gb $2" \
-	-f /project/pom.xml      
+	-f $DIR/../client/project/pom.xml      
 
