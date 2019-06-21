d37beb7b79f77740e548eb288c5b473096b0b4be
victorcuevasv <victorcuevasv@gmail.com>
2019-06-21 17:38:05 +0200

    Update script to use a database other than default.

--- runclient_prestocli.sh
+++ runclient_prestocli.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+#Variables for console output with colors.
+
+red=$'\e[1;31m'
+grn=$'\e[1;32m'
+yel=$'\e[1;33m'
+blu=$'\e[1;34m'
+mag=$'\e[1;35m'
+cyn=$'\e[1;36m'
+end=$'\e[0m'
+
+#$1 Receives the database (schema) name as a parameter.
+
+if [ $# -lt 1 ]; then
+    echo "${yel}Usage: bash runclient_prestocli.sh <dbname>.${end}"
+    exit 0
+fi
+
 docker exec -ti  clientbuildercontainer  /bin/bash -c \
-	"/opt/presto --server namenodecontainer:8080 --catalog hive --schema default"     
+	"/opt/presto --server namenodecontainer:8080 --catalog hive --schema $1"     
 
