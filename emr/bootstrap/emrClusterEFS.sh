1d663e5fb8305324e3aa51f35b42416041475c1d
victorcuevasv <victorcuevasv@gmail.com>
2019-08-20 00:17:55 +0200

    Run full benchmark as a single job.

--- emr/bootstrap/emrClusterEFS.sh
+++ emr/bootstrap/emrClusterEFS.sh
@@ -5,7 +5,8 @@ file_system_id_01=fs-63cc8fc8
 efs_directory=/mnt/efs
 
 sudo mkdir -p ${efs_directory}
-sudo echo "${file_system_id_01}:/ ${efs_directory} efs tls,_netdev" >> /etc/fstab
+#Cannot append to file directly with echo and sudo.
+echo "${file_system_id_01}:/ ${efs_directory} efs tls,_netdev" | sudo tee -a /etc/fstab
 sudo mount -a -t efs defaults
 sudo mkdir ${efs_directory}/data
 sudo chmod -R 777 ${efs_directory}/data
