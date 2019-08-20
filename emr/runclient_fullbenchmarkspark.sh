7b85f223a3ebc29f9c1e93075f5e294bf6c6c56d
victorcuevasv <victorcuevasv@gmail.com>
2019-08-20 16:30:04 +0200

    Run the full benchmark as a single job.

--- emr/runclient_fullbenchmarkspark.sh
+++ emr/runclient_fullbenchmarkspark.sh
@@ -68,10 +68,10 @@ spark-submit --conf spark.eventLog.enabled=true  \
 org.apache.zookeeper:zookeeper:3.4.6 \
 --class org.bsc.dcc.vcv.RunBenchmarkSpark \
 --master yarn --deploy-mode cluster \
-/mnt/efs/FileStore/job-jars/project/targetsparkdatabricks/client-1.0-SNAPSHOT-jar-with-dependencies.jar \
+/mnt/efs/data/FileStore/job-jars/project/targetsparkdatabricks/client-1.0-SNAPSHOT-jar-with-dependencies.jar \
 /mnt/efs/data tpcdsdb$1gb-$2 13ox7IwkFEcRU61h2NXeAaSZMyTRzCby8 sparkemr2nodes sparkemr \
 $2 UNUSED tables _ext s3://tpcds-datasets/$1GB \
-s3://tpcds-warehouse-emr-spark-$1gb-$2 parquet false /mnt/efs/FileStore/job-jars/project/targetsparkdatabricks/client-1.0-SNAPSHOT-jar-with-dependencies.jar false \
+s3://tpcds-warehouse-emr-spark-$1gb-$2 parquet false /mnt/efs/data/FileStore/job-jars/project/targetsparkdatabricks/client-1.0-SNAPSHOT-jar-with-dependencies.jar false \
 UNUSED QueriesSpark results plans true \
 true all true true $3 \
 1954
