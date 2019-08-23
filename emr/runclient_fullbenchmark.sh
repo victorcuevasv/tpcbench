a0e0f1a11bca541b3502d292bd6196ed9685e95c
victorcuevasv <victorcuevasv@gmail.com>
2019-08-23 15:39:57 +0200

    Run full benchmark as a single job.

--- emr/runclient_fullbenchmark.sh
+++ emr/runclient_fullbenchmark.sh
@@ -68,7 +68,7 @@ printf "\n\n%s\n\n" "${mag}Running the full TPC-DS benchmark.${end}"
 #args[28] use multiple connections (true|false)
 
 docker run --network="host" --rm --user $USER_ID:$GROUP_ID --name clientbuildercontainer -ti \
---volume $DIR/../vols/data:/data \
+--volume /mnt/efs/data:/data \
 --volume $DIR/../client/project:/project \
 --entrypoint mvn clientbuilder:dev \
 exec:java -Dexec.mainClass="org.bsc.dcc.vcv.RunBenchmark" \
