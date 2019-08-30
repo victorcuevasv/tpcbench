2861ee0dc65bf0be92f09ebfef4343d1b1f97c9a
Your Name <you@example.com>
2019-08-30 14:05:59 +0000

    Run Presto EMR with MySQL metastore.

--- emr/runclient_analyze_tables.sh
+++ emr/runclient_analyze_tables.sh
@@ -19,15 +19,33 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 #Execute the Java project with Maven on the client builder container running in the docker-compose setup. 
 
+if [ $# -lt 2 ]; then
+    echo "${yel}Usage: bash runclient_analyze_tables.sh <scale factor> <experiment instance number>${end}"
+    exit 0
+fi
+
 printf "\n\n%s\n\n" "${mag}Analyzing the tables in the database.${end}"
 
+#args[0] main work directory
+#args[1] schema (database) name
+#args[2] results folder name (e.g. for Google Drive)
+#args[3] experiment name (name of subfolder within the results folder)
+#args[4] system name (system name used within the logs)
+ 
+#args[5] test name (i.e. load)
+#args[6] experiment instance number
+#args[7] compute statistics for columns (true/false)
+#args[8] hostname of the server
+
 docker run --network="host" --rm --user $USER_ID:$GROUP_ID --name clientbuildercontainer -ti \
---volume $DIR/../vols/data:/data \
+--volume /mnt/efs/data:/data \
+--volume /mnt/efs/FileStore:/FileStore \
 --volume $DIR/../client/project:/project \
 --entrypoint mvn clientbuilder:dev \
-	exec:java -Dexec.mainClass="org.bsc.dcc.vcv.AnalyzeTables" \
-	-Dexec.args="hive $(hostname) false" \
-	-f /project/pom.xml       
-  
+exec:java -Dexec.mainClass="org.bsc.dcc.vcv.AnalyzeTables" \
+-Dexec.args="/data tpcdsdb$1gb_$2 13ox7IwkFEcRU61h2NXeAaSZMyTRzCby8 prestoemr2nodes prestoemr \
+analyze $2 true $(hostname)" \
+-f /project/pom.xml       
 
-  
+
+
---
+++ emr/runclient_createschema.sh
@@ -0,0 +1,37 @@
+#!/bin/bash   
+
+#Variables for console output with colors.
+
+red=$'\e[1;31m'
+grn=$'\e[1;32m'
+yel=$'\e[1;33m'
+blu=$'\e[1;34m'
+mag=$'\e[1;35m'
+cyn=$'\e[1;36m'
+end=$'\e[0m'
+
+#Get the user id of the user executing this script.
+USER_ID=$(id -u)
+#Get the user id of the user executing this script.
+GROUP_ID=$(id -g)
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+if [ $# -lt 3 ]; then
+    echo "${yel}Usage: bash runclient_createschema.sh <system> <scale factor> <experiment instance number> ${end}"
+    exit 0
+fi
+
+#Execute the Java project with Maven on the client builder container running in the docker-compose setup. 
+
+printf "\n\n%s\n\n" "${mag}Creating the schema (database).${end}"  
+
+docker run --network="host" --rm --user $USER_ID:$GROUP_ID --name clientbuildercontainer -ti \
+--volume /mnt/efs/data:/data \
+--volume $DIR/../client/project:/project \
+--entrypoint mvn clientbuilder:dev \
+exec:java -Dexec.mainClass="org.bsc.dcc.vcv.CreateSchema" \
+-Dexec.args="$(hostname) $1 tpcdsdb$2gb_$3" \
+-f /project/pom.xml
+
+
