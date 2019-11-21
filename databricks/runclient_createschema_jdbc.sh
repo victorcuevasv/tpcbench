ae35dd6ee245fc36e314272142e087d6dfdd0a73
Your Name <you@example.com>
2019-11-21 16:18:54 +0100

    Create databases from Databricks JDBC.

---
+++ databricks/runclient_createschema_jdbc.sh
@@ -0,0 +1,35 @@
+#!/bin/bash   
+
+#Variables for console output with colors.
+
+red=$'\e[1;31m'
+grn=$'\e[1;32m'
+yel=$'\e[1;33m'
+blu=$'\e[1;34m'
+mag=$'\e[1;35m'
+cyn=$'\e[1;36m'
+end=$'\e[0m'
+
+#Get the user id of the user executing this script.
+USER_ID=$(id -u)
+#Get the user id of the user executing this script.
+GROUP_ID=$(id -g)
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+if [ $# -lt 2 ]; then
+    echo "${yel}Usage: bash runclient_createschema_jdbc.sh <scale factor> <suffix>${end}"
+    exit 0
+fi
+
+#Execute the Java project with Maven on the client builder container running in the docker-compose setup. 
+
+printf "\n\n%s\n\n" "${mag}Creating the schema (database).${end}"
+
+docker exec -ti --user $USER_ID:$GROUP_ID clientbuildercontainer  /bin/bash -c \
+	"mvn exec:java -Dexec.mainClass=\"org.bsc.dcc.vcv.CreateSchema\" \
+	-Dexec.args=\"dbc-08fc9045-faef.cloud.databricks.com sparkdatabricksjdbc tpcdsdb$1gb-$2\" \
+	-f /project/pom.xml"  
+  
+
+
