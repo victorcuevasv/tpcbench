07b9505e2be9054b514568ed1a7ac79ce1d11fc6
Your Name <you@example.com>
2019-12-10 12:59:48 +0100

    Add script to run concurrency test simulator.

---
+++ runclient_concurrency_mock.sh
@@ -0,0 +1,41 @@
+#!/bin/bash   
+
+#Variables for console output with colors.
+
+red=$'\e[1;31m'
+grn=$'\e[1;32m'
+yel=$'\e[1;33m'
+blu=$'\e[1;34m'
+mag=$'\e[1;35m'
+cyn=$'\e[1;36m'
+end=$'\e[0m'
+
+#Get the user id of the user executing this script.
+USER_ID=$(id -u)
+#Get the user id of the user executing this script.
+GROUP_ID=$(id -g)
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+#Create and populate the database from the .dat files. The scale factor is passed as an argument
+#and used to identify the folder that holds the data.
+#$1 scale factor (positive integer)
+#$2 experiment instance number (positive integer)
+
+if [ $# -lt 1 ]; then
+    echo "${yel}Usage: bash runclient_concurrency_mock.sh <n streams>${end}"     
+    exit 0
+fi
+
+#Execute the Java project with Maven on the client builder container running in the docker-compose setup. 
+
+printf "\n\n%s\n\n" "${mag}Running concurrency test simulation.${end}"       
+
+docker run --network="host" --rm --user $USER_ID:$GROUP_ID --name clientbuildercontainer -ti \
+--volume $DIR/vols/data:/data \
+--volume $DIR/client/project:/project \
+--entrypoint mvn clientbuilder:dev \
+exec:java -Dexec.mainClass="org.bsc.dcc.vcv.prot.ExecuteQueriesConcurrent" \
+-Dexec.args="$1" \
+-f /project/pom.xml
+
