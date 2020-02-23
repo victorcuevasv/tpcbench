9b7db605927328a95093139dc1fd8561efdb76d2
Your Name <you@example.com>
2020-02-23 20:16:31 +0100

    Update script for concurrency.

--- snowflake/runclient_fullbenchmark_conc_limited.sh
+++ snowflake/runclient_fullbenchmark_conc_limited.sh
@@ -25,7 +25,7 @@ GROUP_ID=$(id -g)
 #$4 number of worker threads (positive integer)
 
 if [ $# -lt 4 ]; then
-    echo "${yel}Usage: bash runclient_fullbenchmark.sh <scale factor> <experiment instance number> <number of streams> <number of worker threads>${end}"
+    echo "${yel}Usage: bash runclient_fullbenchmark_conc_limited.sh <scale factor> <experiment instance number> <number of streams> <number of workers>${end}"
     exit 0
 fi
 
@@ -33,42 +33,78 @@ fi
 
 printf "\n\n%s\n\n" "${mag}Running the full TPC-DS benchmark.${end}"
 
+args=()
+
 #args[0] main work directory
+args[0]="/data"
 #args[1] schema (database) name
+args[1]="tpcdsdb$1gb"
 #args[2] results folder name (e.g. for Google Drive)
+args[2]="19aoujv0ull8kx87l4by700xikfythorv"
 #args[3] experiment name (name of subfolder within the results folder)
+args[3]="snowflake-large-$1gb-tput-$3streams-$4workers"
 #args[4] system name (system name used within the logs)
- 
+args[4]="snowflake"
+
 #args[5] experiment instance number
+args[5]="$2"
 #args[6] directory for generated data raw files
+args[6]="UNUSED"
 #args[7] subdirectory within the jar that contains the create table files
+args[7]="tables"
 #args[8] suffix used for intermediate table text files
+args[8]="_ext"
 #args[9] prefix of external location for raw data tables (e.g. S3 bucket), null for none
- 
+args[9]="TPCDSDB$1GB_S3_STAGE/$1GB"
+
 #args[10] prefix of external location for created tables (e.g. S3 bucket), null for none
+args[10]="null"
 #args[11] format for column-storage tables (PARQUET, DELTA)
+args[11]="UNUSED"
 #args[12] whether to run queries to count the tuples generated (true/false)
+args[12]="false"
 #args[13] whether to use data partitioning for the tables (true/false)
+args[13]="false"
 #args[14] hostname of the server
- 
+args[14]="zua56993.snowflakecomputing.com"
+
 #args[15] username for the connection
+args[15]="bsctest"
 #args[16] jar file
+args[16]="/project/target/client-1.0-SNAPSHOT.jar"
 #args[17] whether to generate statistics by analyzing tables (true/false)
+args[17]="false"
 #args[18] if argument above is true, whether to compute statistics for columns (true/false)
+args[18]="UNUSED"
 #args[19] queries dir within the jar
+args[19]="QueriesSnowflake"
 
 #args[20] subdirectory of work directory to store the results
+args[20]="results"
 #args[21] subdirectory of work directory to store the execution plans
+args[21]="plans"
 #args[22] save power test plans (boolean)
+args[22]="false"
 #args[23] save power test results (boolean)
+args[23]="true"
 #args[24] "all" or query file
+args[24]="all"
  
 #args[25] save tput test plans (boolean)
+args[25]="false"
 #args[26] save tput test results (boolean)
+args[26]="true"
 #args[27] number of streams
+args[27]="$3"
 #args[28] random seed
+args[28]="1954"
 #args[29] number of workers
+args[29]="$4"
+
 #args[30] use multiple connections (true|false)
+args[30]="true"
+#args[31] flags (111111 schema|load|analyze|zorder|power|tput)
+args[31]="000001"
 
 docker run --network="host" --rm --user $USER_ID:$GROUP_ID --name clientbuildercontainer -ti \
 --volume /data:/data \
@@ -76,12 +112,7 @@ docker run --network="host" --rm --user $USER_ID:$GROUP_ID --name clientbuilderc
 --volume $DIR/../vols/hive:/temporal \
 --entrypoint mvn clientbuilder:dev \
 exec:java -Dexec.mainClass="org.bsc.dcc.vcv.RunBenchmarkLimit" \
--Dexec.args="/data tpcdsdb$1gb 1pi53t9ak8siwmdi22zrjzu5fk-1b9gvp snowflakeconcurrencylimittest snowflake \
-$2 UNUSED tables _ext TPCDSDB$1GB_S3_STAGE/$1GB \
-null UNUSED false false zua56993.snowflakecomputing.com \
-bsctest /project/target/client-1.0-SNAPSHOT.jar false UNUSED QueriesSnowflake \
-results plans false true all \
-false true $3 1954 $4 false" \
+-Dexec.args="${args[*]}" \
 -f /project/pom.xml
 
 
