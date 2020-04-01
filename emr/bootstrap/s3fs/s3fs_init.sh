681df4b6b2c06242aed1fe2bd51fede0f80fc173
Your Name <you@example.com>
2020-04-01 17:17:00 +0200

    Update bootstrap script.

--- emr/bootstrap/s3fs/s3fs_init.sh
+++ emr/bootstrap/s3fs/s3fs_init.sh
@@ -2,6 +2,8 @@
 
 #User to own the directories.
 mUSER="ec2-user"
+#Buckets to mount.
+buckets=("tpcds-jars" "tpcds-results-test")
 
 #Install s3fs
 sudo yum install -y gcc libstdc++-devel gcc-c++ fuse fuse-devel curl-devel libxml2-devel mailcap automake openssl-devel git
@@ -11,9 +13,12 @@ git clone https://github.com/s3fs-fuse/s3fs-fuse
 make -C ./s3fs-fuse
 sudo make -C ./s3fs-fuse install
 
-#Mount the bucket
-mkdir /home/$mUSER/s3-drive
-sudo chown $mUSER:$mUSER -R /home/$mUSER/s3-drive
-sudo -u $mUSER s3fs -o iam_role="tpcds-mount" -o url="https://s3-us-west-2.amazonaws.com" \
--o endpoint=us-west-2 -o dbglevel=info -o curldbg \
--o use_cache=/tmp tpcds-temporal /home/$mUSER/s3-drive
+#Mount the buckets.
+for bucket in "${buckets[@]}" ; do
+	mkdir /home/$mUSER/$bucket
+	sudo chown $mUSER:$mUSER -R /home/$mUSER/$bucket
+	sudo -u $mUSER s3fs -o iam_role="tpcds-mount" -o url="https://s3-us-west-2.amazonaws.com" \
+	-o endpoint=us-west-2 -o dbglevel=info -o curldbg \
+	-o use_cache=/tmp $bucket /home/$mUSER/$bucket
+done
+
