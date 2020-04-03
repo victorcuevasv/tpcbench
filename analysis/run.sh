22d3c45433e1656cde89c3b57e9a2b20c32eb016
Your Name <you@example.com>
2020-04-03 17:04:49 +0200

    Add user id to run script.

--- analysis/run.sh
+++ analysis/run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+#Get the user id of the user executing this script.
+USER_ID=$(id -u)
+#Get the user id of the user executing this script.
+GROUP_ID=$(id -g)
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-docker run -e PASSWORD=1234 --rm -p 8787:8787 -v $DIR/Documents:/home/rstudio/Documents rstudio:dev
+docker run --user $USER_ID:$GROUP_ID -e PASSWORD=1234 --rm -p 8787:8787 -v $DIR/Documents:/home/rstudio/Documents rstudio:dev
 
