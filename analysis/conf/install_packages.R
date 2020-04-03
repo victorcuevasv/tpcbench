8892027cbea8abe9d3457110295b59ba4dda684f
Your Name <you@example.com>
2020-04-03 16:24:39 +0200

    Add packages necessary for AWS.

--- analysis/conf/install_packages.R
+++ analysis/conf/install_packages.R
@@ -1,3 +1,5 @@
-
+install.packages("base64enc")
+install.packages("curl")
+install.packages("httr")
 install.packages("aws.s3", repos = c("cloudyr" = "http://cloudyr.github.io/drat"), INSTALL_opts = "--no-multiarch")
 
