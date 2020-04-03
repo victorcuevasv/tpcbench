1777a07b02dd7ec7356d830bb697f9d3b9473d6b
Your Name <you@example.com>
2020-04-03 17:47:34 +0200

    Add stringr library.

--- analysis/conf/install_packages.R
+++ analysis/conf/install_packages.R
@@ -3,6 +3,8 @@ install.packages("curl")
 install.packages("httr")
 install.packages("aws.s3", repos = c("cloudyr" = "http://cloudyr.github.io/drat"), INSTALL_opts = "--no-multiarch")
 
+install.packages("stringr")
+
 library("rio")
 
 install_formats()
