ac670258ab5fd4cf40b9ba46cbb5d5da939b956a
Your Name <you@example.com>
2020-04-04 18:30:37 +0200

    Add command to mount bucket.

--- analysis/run_script.sh
+++ analysis/run_script.sh
@@ -11,5 +11,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 docker run --rm --privileged --user $USER_ID:$GROUP_ID \
 -v $DIR/Documents:/home/rstudio/Documents  \
 -v $DIR/Output:/home/rstudio/Output  \
---entrypoint Rscript rstudio:dev --version   
+--entrypoint /bin/bash rstudio:dev -c \
+"mkdir /home/rstudio/Documents/tpcds-results-test; s3fs tpcds-results-test /home/rstudio/Documents/tpcds-results-test; Rscript /home/rstudio/Documents/stacked_chart.R"        
+
+#--entrypoint Rscript rstudio:dev --version   
 
