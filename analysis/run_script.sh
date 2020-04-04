5fcda2a3c048e9e7bd7fa4966977b2e96d6fe299
Your Name <you@example.com>
2020-04-04 18:41:58 +0200

    Change path of mounted buckets.

--- analysis/run_script.sh
+++ analysis/run_script.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-#Get the user id of the user executing this script.
-USER_ID=$(id -u)
-#Get the user id of the user executing this script.
-GROUP_ID=$(id -g)
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 
-docker run --rm --privileged --user $USER_ID:$GROUP_ID \
+docker run --rm --privileged \
 -v $DIR/Documents:/home/rstudio/Documents  \
 -v $DIR/Output:/home/rstudio/Output  \
 --entrypoint /bin/bash rstudio:dev -c \
-"mkdir /home/rstudio/Documents/tpcds-results-test; s3fs tpcds-results-test /home/rstudio/Documents/tpcds-results-test; Rscript /home/rstudio/Documents/stacked_chart.R"        
-
-#--entrypoint Rscript rstudio:dev --version   
-
+"mkdir -p /home/rstudio/s3buckets/tpcds-results-test; s3fs tpcds-results-test /home/rstudio/s3buckets/tpcds-results-test; Rscript /home/rstudio/Documents/stacked_chart.R"        
+ 
