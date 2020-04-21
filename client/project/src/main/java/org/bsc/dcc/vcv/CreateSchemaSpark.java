15276f54917dad47dd711b0ad983c9dd24871ab5
Your Name <you@example.com>
2020-04-21 14:15:08 +0200

    Add constructor using commons cli.

--- client/project/src/main/java/org/bsc/dcc/vcv/CreateSchemaSpark.java
+++ client/project/src/main/java/org/bsc/dcc/vcv/CreateSchemaSpark.java
@@ -3,31 +3,51 @@ package org.bsc.dcc.vcv;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.spark.sql.SparkSession;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
 
 public class CreateSchemaSpark {
 
-	protected static final Logger logger = LogManager.getLogger("AllLog");
-	protected final String system;
-	protected final String dbName;
-	protected SparkSession spark;
-
-	public CreateSchemaSpark() {
-		
+	private static final Logger logger = LogManager.getLogger("AllLog");
+	private final String system;
+	private final String dbName;
+	private SparkSession spark;
+	
+	public CreateSchemaSpark(CommandLine commandLine) {
+		try {
+			this.spark = SparkSession.builder().appName("TPC-DS Database Creation")
+						.getOrCreate();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			this.logger.error("Error in CreateDatabaseSpark constructor.");
+			this.logger.error(e);
+			this.logger.error(AppUtil.stringifyStackTrace(e));
+		}
+		this.system = commandLine.getOptionValue("system-name");
+		this.dbName = commandLine.getOptionValue("schema-name");
 	}
 	
+	/**
+	 * @param args
+	 * 
+	 * args[0] system used to create the schema on the metastore
+	 * args[1] schema (database) name
+	 */
 	public CreateSchemaSpark(String[] args) {
+		if( args.length != 2 ) {
+			System.out.println("Incorrect number of arguments.");
+			logger.error("Insufficient arguments.");
+			System.exit(1);
+		}
 		this.system = args[0];
 		this.dbName = args[1];
 		try {
-			if( this.system.equals("sparkdatabricks") ) {
-				this.spark = SparkSession.builder().appName("TPC-DS Database Creation")
+			this.spark = SparkSession.builder().appName("TPC-DS Database Creation")
 						.getOrCreate();
-			}
-			else {
-				this.spark = SparkSession.builder().appName("TPC-DS Database Creation")
-						.enableHiveSupport()
-						.getOrCreate();
-			}
 		}
 		catch(Exception e) {
 			e.printStackTrace();
@@ -37,26 +57,36 @@ public class CreateSchemaSpark {
 		}
 	}
 
-	/**
-	 * @param args
-	 * 
-	 * args[0] system used to create the schema on the metastore
-	 * args[1] schema (database) name
-	 */
 	public static void main(String[] args) {
-		if( args.length != 2 ) {
-			System.out.println("Incorrect number of arguments.");
-			logger.error("Insufficient arguments.");
-			System.exit(1);
+		CreateSchemaSpark application = null;
+		//Check is GNU-like options are used.
+		boolean gnuOptions = args[0].contains("--") ? true : false;
+		if( ! gnuOptions )
+			application = new CreateSchemaSpark(args);
+		else {
+			CommandLine commandLine = null;
+			try {
+				RunBenchmarkSparkOptions runOptions = new RunBenchmarkSparkOptions();
+				Options options = runOptions.getOptions();
+				CommandLineParser parser = new DefaultParser();
+				commandLine = parser.parse(options, args);
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+				this.logger.error("Error in CreateDatabaseSparkCLI main.");
+				this.logger.error(e);
+				this.logger.error(AppUtil.stringifyStackTrace(e));
+				System.exit(1);
+			}
+			application = new CreateSchemaSpark(commandLine);
 		}
-		CreateSchemaSpark prog = new CreateSchemaSpark(args);
-		prog.createSchema();
-		//if( ! args[0].equals("sparkdatabricks") ) {
-		//	prog.closeConnection();
-		//}
+		application.createSchema();
+		if( ! application.system.equals("sparkdatabricks") ) {
+			application.closeConnection();
+		}
 	}
 
-	protected void createSchema() {
+	private void createSchema() {
 		try {
 			System.out.println("Creating schema (database) " + this.dbName + " with " + this.system);
 			this.logger.info("Creating schema (database) " + this.dbName + " with " + this.system);
@@ -70,7 +100,7 @@ public class CreateSchemaSpark {
 		}
 	}
 	
-	protected void closeConnection() {
+	private void closeConnection() {
 		try {
 			this.spark.stop();
 		}
