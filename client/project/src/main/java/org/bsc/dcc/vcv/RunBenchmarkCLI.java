39c304b13dcdf9db7faa017f6047a92fc3cee96d
Your Name <you@example.com>
2020-04-26 20:45:47 +0200

    Fix bug in saving results for EMR Presto.

--- client/project/src/main/java/org/bsc/dcc/vcv/RunBenchmarkCLI.java
+++ client/project/src/main/java/org/bsc/dcc/vcv/RunBenchmarkCLI.java
@@ -111,7 +111,7 @@ public class RunBenchmarkCLI {
 				this.executeCommand("mkdir -p /dbfs/mnt/tpcds-results-test/" + this.resultsDir);
 				this.executeCommand("cp -r " + this.workDir + "/" + this.resultsDir + "/* /dbfs/mnt/tpcds-results-test/" + this.resultsDir + "/");
 			}
-			else if( this.system.equals("sparkemr")  ) {
+			else if( this.system.equals("sparkemr") || this.system.equals("prestoemr")  ) {
 				this.executeCommand("mkdir -p /mnt/tpcds-results-test/" + this.resultsDir);
 				this.executeCommand("cp -r " + this.workDir + "/" + this.resultsDir + "/* /mnt/tpcds-results-test/" + this.resultsDir + "/");
 			}
