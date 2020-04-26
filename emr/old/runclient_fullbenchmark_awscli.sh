a91a5bbed93b040b53b7e861a1987a2ec0bb33f4
Your Name <you@example.com>
2020-04-26 19:12:05 +0200

    Update script to run EMR Presto for commons cli options.

--- emr/old/runclient_fullbenchmark_awscli.sh
+++ emr/old/runclient_fullbenchmark_awscli.sh
@@ -26,11 +26,9 @@ if [ $# -lt 3 ]; then
     exit 0
 fi
 
-DirNameWarehouse="tpcds-warehouse-prestoemr-529-$1gb-$2"
-DirNameResults="1odwczxc3jftmhmvahdl7tz32dyyw0pen"
-DatabaseName="tpcds_prestoemr_529_$1gb_$2_db"
-Nodes="2"
-JarFile="/mnt/tpcds-jars/targetemr/client-1.2-SNAPSHOT-SHADED.jar"
+BucketName="tpcds-warehouse-prestoemr-529-$1gb-$2"
+DatabaseName="tpcds_prestoemr529_$1gb_$2_db"
+Nodes="8"
 
 printf "\n\n%s\n\n" "${mag}Running the full TPC-DS benchmark.${end}"
 
@@ -38,13 +36,13 @@ RUN_CREATE_BUCKET=0
 
 if [ "$RUN_CREATE_BUCKET" -eq 1 ]; then
     #Create the bucket
-    aws s3api create-bucket --bucket $DirNameWarehouse --region us-west-2 --create-bucket-configuration LocationConstraint=us-west-2
+    aws s3api create-bucket --bucket $BucketName --region us-west-2 --create-bucket-configuration LocationConstraint=us-west-2
     #Add the Owner tag
-    aws s3api put-bucket-tagging --bucket $DirNameWarehouse --tagging 'TagSet=[{Key=Owner,Value=eng-benchmarking@databricks.com}]'
+    aws s3api put-bucket-tagging --bucket $BucketName --tagging 'TagSet=[{Key=Owner,Value=eng-benchmarking@databricks.com}]'
     #Block all public access for the bucket
-    aws s3api put-public-access-block --bucket $DirNameWarehouse --public-access-block-configuration "BlockPublicAcls=true,IgnorePublicAcls=true,BlockPublicPolicy=true,RestrictPublicBuckets=true"  
+    aws s3api put-public-access-block --bucket $BucketName --public-access-block-configuration "BlockPublicAcls=true,IgnorePublicAcls=true,BlockPublicPolicy=true,RestrictPublicBuckets=true"  
     #Create and empty folder to enable mounting
-    aws s3api put-object --bucket $DirNameWarehouse --key empty
+    aws s3api put-object --bucket $BucketName --key empty
     #Create the table folders
     tables=(call_center catalog_page catalog_returns catalog_sales customer customer_address \
 			customer_demographics date_dim dbgen_version household_demographics income_band \
@@ -53,7 +51,7 @@ if [ "$RUN_CREATE_BUCKET" -eq 1 ]; then
 	for t in "${tables[@]}"
 	do
 		#Must specify through the content type that it is a directory and use the trailing slash.
-		aws s3api put-object --bucket $DirNameWarehouse --content-type application/x-directory --key $t/
+		aws s3api put-object --bucket $BucketName --content-type application/x-directory --key $t/
 	done
     exit 0
 fi
@@ -62,53 +60,82 @@ RUN_DELETE_BUCKET=0
 
 if [ "$RUN_DELETE_BUCKET" -eq 1 ]; then
     #Delete the bucket
-    aws s3 rb s3://$DirNameWarehouse --force
+    aws s3 rb s3://$BucketName --force
     exit 0
 fi
 
 args=()
 
-#main work directory
-args[0]="--main-work-dir=/data"
-#schema (database) name
-args[1]="--schema-name=$DatabaseName"
-#results folder name (e.g. for Google Drive)
-args[2]="--results-dir=$DirNameResults"
-#experiment name (name of subfolder within the results folder)
-args[3]="--experiment-name=prestoemr-529-${Nodes}nodes-$1gb-experimental"
-#system name (system name used within the logs)
-args[4]="--system-name=prestoemr"
+#args[0] main work directory
+args[0]="/mnt/efs/scratch/hadoop/data"
+#args[1] schema (database) name
+args[1]="$DatabaseName"
+#args[2] results folder name (e.g. for Google Drive)
+args[2]="19aoujv0ull8kx87l4by700xikfythorv"
+#args[3] experiment name (name of subfolder within the results folder)
+args[3]="prestoemr-529-${Nodes}nodes-$1gb"
+#args[4] system name (system name used within the logs)
+args[4]="prestoemr"
 
-#experiment instance number
-args[5]="--instance-number=$2"
-#prefix of external location for raw data tables (e.g. S3 bucket), null for none
-args[6]="--ext-raw-data-location=s3://tpcds-datasets/$1GB"
-#prefix of external location for created tables (e.g. S3 bucket), null for none
-args[7]="--ext-tables-location=s3://tpcds-warehouses-test/$DirNameWarehouse"
-#format for column-storage tables (PARQUET, DELTA)
-args[8]="--table-format=orc"
-#whether to use data partitioning for the tables (true/false)
-args[9]="--use-partitioning=false"
+#args[5] experiment instance number
+args[5]="$2"
+#args[6] directory for generated data raw files
+args[6]="UNUSED"
+#args[7] subdirectory within the jar that contains the create table files
+args[7]="tables"
+#args[8] suffix used for intermediate table text files
+args[8]="_ext"
+#args[9] prefix of external location for raw data tables (e.g. S3 bucket), null for none
+args[9]="s3://tpcds-datasets/$1GB"
 
-#jar file
-args[10]="--jar-file=$JarFile"
-#whether to generate statistics by analyzing tables (true/false)
-args[11]="--use-row-stats=true"
-#if argument above is true, whether to compute statistics for columns (true/false)
-args[12]="--use-column-stats=true"
-#"all" or query file
-args[13]="--all-or-query-file=query2.sql" 
-#number of streams
-args[14]="--number-of-streams=$3"
+#args[10] prefix of external location for created tables (e.g. S3 bucket), null for none
+args[10]="s3://$BucketName"
+#args[11] format for column-storage tables (PARQUET, DELTA)
+args[11]="orc"
+#args[12] whether to run queries to count the tuples generated (true/false)
+args[12]="false"
+#args[13] whether to use data partitioning for the tables (true/false)
+args[13]="false"
+#args[14] whether to use bucketing for Hive and Presto
+args[14]="true"
 
-#flags (110000 schema|load|analyze|zorder|power|tput)
-args[15]="--execution-flags=111011"
-#whether to use bucketing for Hive and Presto
-args[16]="--use-bucketing=false"
-#hostname of the server
-args[17]="--server-hostname=localhost"
-#username for the connection
-args[18]="--connection-username=hadoop"
+#args[15] hostname of the server
+args[15]="localhost"
+#args[16] username for the connection
+args[16]="hadoop"
+#args[17] jar file
+args[17]="/mnt/efs/FileStore/job-jars/project/targetemr/client-1.2-SNAPSHOT-jar-with-dependencies.jar"
+#args[18] whether to generate statistics by analyzing tables (true/false)
+args[18]="true"
+#args[19] if argument above is true, whether to compute statistics for columns (true/false)
+args[19]="true"
+
+#args[20] queries dir within the jar
+args[20]="QueriesPresto"
+#args[21] subdirectory of work directory to store the results
+args[21]="results"
+#args[22] subdirectory of work directory to store the execution plans
+args[22]="plans"
+#args[23] save power test plans (boolean)
+args[23]="true"
+#args[24] save power test results (boolean)
+args[24]="true"
+
+#args[25] "all" or query file
+args[25]="all" 
+#args[26] save tput test plans (boolean)
+args[26]="true"
+#args[27] save tput test results (boolean)
+args[27]="true"
+#args[28] number of streams
+args[28]="$3"
+#args[29] random seed
+args[29]="1954"
+
+#args[30] use multiple connections (true|false)
+args[30]="true"
+#args[31] flags (111111 schema|load|analyze|zorder|power|tput)
+args[31]="111011"
 
 function json_string_list() {
     declare array=("$@")
@@ -147,7 +174,7 @@ steps_func()
       ],
       "Type":"CUSTOM_JAR",
       "ActionOnFailure":"TERMINATE_CLUSTER",
-      "Jar":"$JarFile",
+      "Jar":"/mnt/efs/FileStore/job-jars/project/targetemr/client-1.2-SNAPSHOT-jar-with-dependencies.jar",
       "Properties":"",
       "Name":"Custom JAR"
    }
@@ -214,15 +241,15 @@ configurations_func()
 EOF
 }
 
+
 bootstrap-actions_func()
 {
   cat <<EOF
 [
    {
-      "Path":"s3://bsc-bootstrap/s3fs/emr_init.sh",
+      "Path":"s3://bsc-bootstrap/emrClusterEFS_user_param.sh",
       "Args":[
-         "hadoop",
-         "tpcds-jars,tpcds-results-test"
+         "hadoop"
       ],
       "Name":"Custom action"
    }
