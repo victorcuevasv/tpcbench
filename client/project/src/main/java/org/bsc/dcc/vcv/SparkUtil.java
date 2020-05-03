331bd6171b1d9df5c9669826b3df7f1fd24027b3
Your Name <you@example.com>
2020-05-03 16:22:50 +0200

    Order the spark configuration log.

--- client/project/src/main/java/org/bsc/dcc/vcv/SparkUtil.java
+++ client/project/src/main/java/org/bsc/dcc/vcv/SparkUtil.java
@@ -1,9 +1,10 @@
 package org.bsc.dcc.vcv;
 
+import java.util.List;
+import java.util.ArrayList;
 import scala.collection.immutable.Map;
 import scala.collection.*;
 import scala.Tuple2;
-
 import org.apache.spark.sql.SparkSession;
 
 public class SparkUtil {
@@ -11,19 +12,21 @@ public class SparkUtil {
 	public static String stringifySparkConfiguration(SparkSession spark) {
 		scala.collection.immutable.Map<String, String> map = spark.conf().getAll();
 		scala.collection.Iterator<Tuple2<String, String>> iter = map.iterator();
-		StringBuilder sb = new StringBuilder();
+		List<String> list = new ArrayList<String>();
 		while (iter.hasNext()) {
 		    Tuple2<String, String> entry = iter.next();
+		    StringBuilder sb = new StringBuilder();
 		    sb.append(entry._1);
-		    sb.append('=').append('"');
+		    sb.append('=');
 		    sb.append(entry._2);
-		    sb.append('"');
-		    if (iter.hasNext()) {
-		        sb.append('\n');
-		        sb.append('\n');
-		    }
+		    list.add(sb.toString());
 		}
-		return sb.toString();
+		list.sort(String.CASE_INSENSITIVE_ORDER);
+		StringBuilder builder = new StringBuilder();
+		for(String s : list) {
+			builder.append(s + "\n\n");
+		}
+		return builder.toString();
 	}
 
 }
