f42790c0285ae802c75101a90e6addf724cad449
Your Name <you@example.com>
2020-09-04 04:09:45 +0200

    Remove empty payload.

--- client/project/src/main/java/org/bsc/dcc/vcv/UpdateDatabaseSparkForceCompaction.java
+++ client/project/src/main/java/org/bsc/dcc/vcv/UpdateDatabaseSparkForceCompaction.java
@@ -217,8 +217,6 @@ public class UpdateDatabaseSparkForceCompaction {
 			.write()
 			.format("org.apache.hudi")
 			.option("hoodie.datasource.write.operation", "upsert")
-			.option("hoodie.datasource.write.payload.class", 
-					"org.apache.hudi.common.model.EmptyHoodieRecordPayload")
 			.options(hudiOptions)
 			.mode(SaveMode.Append)
 			.save(this.extTablePrefixCreated.get() + "/" + tableName + "_denorm_hudi" + "/");
