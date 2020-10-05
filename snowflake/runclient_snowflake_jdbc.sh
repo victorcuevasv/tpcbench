373881c1476fd063e2f15989b23323f43bf272f9
Your Name <you@example.com>
2020-10-05 04:00:48 +0200

    Fix errors in copy to s3 command.

--- snowflake/runclient_snowflake_jdbc.sh
+++ snowflake/runclient_snowflake_jdbc.sh
@@ -105,7 +105,7 @@ if [ "$RUN_RUN_BENCHMARK" -eq 1 ]; then
 fi
 
 if [ "$COPY_RESULTS_TO_S3" -eq 1 ]; then
-	aws s3 cp --recursive $DIR/../vols/${args[0]}/${args[2]}/ s3://tpcds-results-test/${args[2]}/
+	aws s3 cp --recursive $DIR/../vols/data/$DirNameResults/ s3://tpcds-results-test/$DirNameResults/
 fi
 
 
