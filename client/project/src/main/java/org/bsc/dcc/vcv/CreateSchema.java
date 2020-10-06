ce587074fe462191c4aaa1209861f7367e1a0973
Francisco Javier Jurado Moreno <beetelbrox@gmail.com>
2020-10-06 12:32:27 +0200

    Print DBR token

--- client/project/src/main/java/org/bsc/dcc/vcv/CreateSchema.java
+++ client/project/src/main/java/org/bsc/dcc/vcv/CreateSchema.java
@@ -94,6 +94,7 @@ public class CreateSchema {
 				//String dbrToken = "dapifd4db58404ae64629dc7b41d57f3a769";
 				String dbrToken = AWSUtil.getValue("DatabricksToken");
 				Class.forName(databricksDriverName);
+				System.out.println("DBR TOKEN: " + dbrToken);
 				this.con = DriverManager.getConnection("jdbc:spark://"
 					+ this.hostname + ":443/" + this.dbName
 					+ ";transportMode=http;ssl=1;AuthMech=3"
