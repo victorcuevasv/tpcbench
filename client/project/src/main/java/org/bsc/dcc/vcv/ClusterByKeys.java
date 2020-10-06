724d1359173e02f8c2172fa77c43040731c5256b
Your Name <you@example.com>
2020-10-06 16:52:15 +0200

    Use single level of clustering for Snowflake.

--- client/project/src/main/java/org/bsc/dcc/vcv/ClusterByKeys.java
+++ client/project/src/main/java/org/bsc/dcc/vcv/ClusterByKeys.java
@@ -11,27 +11,27 @@ public class ClusterByKeys {
 		this.map = new HashMap<String, String>();
 		map.put("call_center", "cc_call_center_sk");
 		map.put("catalog_page", "cp_catalog_page_sk");
-		map.put("catalog_returns", "cr_returned_date_sk, cr_item_sk");
-		map.put("catalog_sales", "cs_sold_date_sk, cs_item_sk");
+		map.put("catalog_returns", "cr_returned_date_sk");
+		map.put("catalog_sales", "cs_sold_date_sk");
 		map.put("customer", "c_customer_sk");
 		map.put("customer_address", "ca_address_sk");
 		map.put("customer_demographics", "cd_demo_sk");
 		map.put("date_dim", "d_date_sk");
 		map.put("household_demographics", "hd_demo_sk");
 		map.put("income_band", "ib_income_band_sk");
-		map.put("inventory", "inv_date_sk, inv_item_sk");
+		map.put("inventory", "inv_date_sk");
 		map.put("item", "i_item_sk");
 		map.put("promotion", "p_promo_sk");
 		map.put("reason", "r_reason_sk");
 		map.put("ship_mode", "sm_ship_mode_sk");
 		map.put("store", "s_store_sk");
-		map.put("store_returns", "sr_returned_date_sk, sr_item_sk");
-		map.put("store_sales", "ss_sold_date_sk, ss_item_sk");
+		map.put("store_returns", "sr_returned_date_sk");
+		map.put("store_sales", "ss_sold_date_sk");
 		map.put("time_dim", "t_time_sk");
 		map.put("warehouse", "w_warehouse_sk");
 		map.put("web_page", "wp_web_page_sk");
-		map.put("web_returns", "wr_returned_date_sk, wr_item_sk");
-		map.put("web_sales", "ws_sold_date_sk, ws_item_sk");
+		map.put("web_returns", "wr_returned_date_sk");
+		map.put("web_sales", "ws_sold_date_sk");
 		map.put("web_site", "web_site_sk");
 	}
 	
