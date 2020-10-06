223a96611181a0418127b4cae5c559164a6929c3
Francisco Javier Jurado Moreno <beetelbrox@gmail.com>
2020-10-06 20:06:29 +0200

    Clean script, add row count and pipe as delimiter

--- emr/runclient_redshift_jdbc.sh
+++ emr/runclient_redshift_jdbc.sh
@@ -40,12 +40,6 @@ DirNameResults="redshift"
 DatabaseName="tpcds_redshift_$1gb_$2_${Tag}"
 JarFile="/mnt/tpcds-jars/target/client-1.2-SNAPSHOT-SHADED.jar"
 
-CLUSTER_NAME="TPC-DS_${Tag}_$2"
-RUN_CREATE_CLUSTER=0
-cluster_id=""
-RUN_RUN_BENCHMARK=1
-RUN_TERMINATE_CLUSTER=0
-
 args=()
 
 #main work directory
@@ -92,96 +86,16 @@ args[13]="--all-or-query-file=all"
 #flags (110000 schema|load|analyze|zorder|power|tput)
 args[15]="--execution-flags=110010"
 
-#Wait until the cluster is in a given state by polling using the Databricks CLI.
-#$1 cluster_id
-#$2 state waiting for
-#$3 pause in seconds for polling
-wait_for_state() {
-   declare state=$(get_cluster_state $1)
-   while [ $state != "$2"  ] ;
-   do
-      sleep $3
-      state=$(get_cluster_state $1)
-   done
-}
+args[16]="--count-queries=true"
+args[17]="--raw-column-delimiter=PIPE"
 
-#Get the state (PENDING, RUNNING, TERMINATED) of a cluster via its cluster_id using the Databricks CLI.
-#$1 run_id
-get_cluster_state() {
-	declare jsonStr=""
-	jsonStr=$(databricks clusters get --cluster-id $1)
-	declare state=$(jq -j '.state'  <<<  "$jsonStr")
-	echo $state
-}
-
-post_data_func()
-{
-  cat <<EOF
-{
-    "num_workers": $Nodes,
-    "cluster_name":"$CLUSTER_NAME",
-    "spark_version": "${MajorVersion}.${MinorVersion}.${ScalaVersion}",
-    "spark_conf": {
-        "spark.databricks.delta.autoCompact.enabled": "true",
-        "spark.sql.crossJoin.enabled": "true",
-        "spark.databricks.delta.optimizeWrite.enabled": "true",
-        "spark.sql.broadcastTimeout": "7200"
-    },
-    "aws_attributes": {
-        "first_on_demand": 0,
-        "availability": "ON_DEMAND",
-        "zone_id": "us-west-2b",
-        "spot_bid_price_percent": 100,
-        "ebs_volume_count": 0
-    },
-    "node_type_id": "i3.2xlarge",
-    "ssh_public_keys": [
-        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDjxM9WRCj8vI0aDAVhVi3DkibIaChqwjshgcPvXcth5l1cWxBZLkDdL1CCLSAbUGGL+HX79FL7L46wBCOHTJ2hhd7tjxxDG7IeH/G2Q1wPsTMt7Vpswc8Ijp6BKzbqAJS9HJAq9VPjh0x39gPd2x4vHiRpudKA+RFvTQ1jWRz0nTxI/eteZWB03jrPQxbZFo5v/29VDTwRlDBraC5q3hblfXAUk8cWQkhlFz2XagPNzsigVsY/zJvIJ/zW5ZpPI5El2VK3CEGjqbg5qt7QnIiRydly3N2eWHDIwZM3nfAMYdWig+65U8LOy9NC8J6dk8v/ZlstoOLNNm5+LSkmj9b7 pristine@al-1001"
-    ],
-    "custom_tags": {},
-    "spark_env_vars": {},
-    "enable_elastic_disk": false,
-    "init_scripts": []
-}
-EOF
-}
-
-if [ "$RUN_CREATE_CLUSTER" -eq 1 ]; then
-	echo "${blu}Creating cluster for benchmark execution.${end}"
-	#Must add the quotes to post_data_func to avoid error.
-	jsonClusterCreate=$(databricks clusters create --json "$(post_data_func)")
-	cluster_id=$(jq -j '.cluster_id'  <<<  "$jsonClusterCreate")
-	echo "${blu}Launched creation of cluster with id ${cluster_id}.${end}"
-	echo "${blu}Waiting for the initialization of cluster with id ${cluster_id}.${end}"
-	wait_for_state $cluster_id "RUNNING" 60
-	echo "${blu}Cluster running.${end}"
-fi
-
-#add the cluster id to the parameters
-args[16]="--cluster-id=$cluster_id"
 paramsStr="${args[@]}"
 
-if [ "$RUN_RUN_BENCHMARK" -eq 1 ]; then
-	docker run --network="host" --rm --user $USER_ID:$GROUP_ID --name clientbuildercontainer -ti \
-	--volume $DIR/../vols/data:/data \
-	--volume $DIR/../client/project:/project \
-	--volume $HOME/tpcds-jars:/mnt/tpcds-jars \
-	--entrypoint mvn clientbuilder:dev \
-	exec:java -Dexec.mainClass="org.bsc.dcc.vcv.RunBenchmarkCLI" \
-	-Dexec.args="$paramsStr" \
-	-f /project/pom.xml
-fi
-
-if [ "$RUN_TERMINATE_CLUSTER" -eq 1 ]; then
-	echo "${blu}Terminating cluster used for benchmark execution.${end}"
-	#Must add the quotes to post_data_func to avoid error.
-	databricks clusters delete --cluster-id $cluster_id
-	echo "${blu}Launched termination of cluster with id ${cluster_id}.${end}"
-	echo "${blu}Waiting for the termination of cluster with id ${cluster_id}.${end}"
-	wait_for_state $cluster_id "TERMINATED" 20
-	echo "${blu}Cluster terminated.${end}"
-fi
-
-
-
-
+docker run --network="host" --rm --user $USER_ID:$GROUP_ID --name clientbuildercontainer -ti \
+--volume $DIR/../vols/data:/data \
+--volume $DIR/../client/project:/project \
+--volume $HOME/tpcds-jars:/mnt/tpcds-jars \
+--entrypoint mvn clientbuilder:dev \
+exec:java -Dexec.mainClass="org.bsc.dcc.vcv.RunBenchmarkCLI" \
+-Dexec.args="$paramsStr" \
+-f /project/pom.xml
