191ffdc54747861e3a20c3f57e73eddd53fdc60b
Francisco Javier Jurado Moreno <beetelbrox@gmail.com>
2020-10-11 16:18:02 +0200

    Update endpoint id

--- client/project/src/main/java/org/bsc/dcc/vcv/CreateDatabase.java
+++ client/project/src/main/java/org/bsc/dcc/vcv/CreateDatabase.java
@@ -226,7 +226,7 @@ public class CreateDatabase {
 				this.con = DriverManager.getConnection("jdbc:spark://"
 					+ this.hostname + ":443/" + this.dbName
 					+ ";transportMode=http;ssl=1;AuthMech=3"
-					+ ";httpPath=/sql/1.0/endpoints/a57e3bc75ae9786b"
+					+ ";httpPath=/sql/1.0/endpoints/930f6d130b29ff01"
 					+ ";UID=token;PWD=" + dbrToken
 					+ ";UseNativeQuery=1");
 			}
