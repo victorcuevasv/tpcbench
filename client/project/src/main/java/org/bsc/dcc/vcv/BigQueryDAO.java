587f174ae5052a89570e12b0b0fa1b92f21a9333
Your Name <you@example.com>
2020-10-18 19:08:29 +0200

    Create initial DAO for Bigquery.

---
+++ client/project/src/main/java/org/bsc/dcc/vcv/BigQueryDAO.java
@@ -0,0 +1,45 @@
+package org.bsc.dcc.vcv;
+
+import com.google.cloud.bigquery.BigQuery;
+import com.google.cloud.bigquery.BigQueryException;
+import com.google.cloud.bigquery.BigQueryOptions;
+import com.google.cloud.bigquery.Job;
+import com.google.cloud.bigquery.JobInfo;
+import com.google.cloud.bigquery.QueryJobConfiguration;
+
+public class BigQueryDAO {
+	
+	private final String project;
+    private final String dataset;
+    private final BigQuery bigQuery;
+    
+
+    public BigQueryDAO(String project, String dataset) {
+    	this.project = project;
+    	this.dataset = dataset;
+    	BigQuery bigquery = BigQueryOptions.newBuilder().setProjectId(this.project)
+				.build().getService();
+    }
+
+	public void createTable(String sqlStmt) throws BigQueryException, InterruptedException {
+		try {
+			QueryJobConfiguration config = QueryJobConfiguration.newBuilder(sqlStmt)
+					.setDefaultDataset("tpcds_synapse_1gb_1_1602805814").build();      
+			Job job = bigquery.create(JobInfo.of(config));
+			job = job.waitFor();
+			if (job.isDone()) {
+				System.out.println("Bigquery table created successfully.");
+			}
+			else {
+				System.out.println("Bigquery table was not created.");
+			}
+		}
+		catch (BigQueryException | InterruptedException e) {
+			throw e;
+		}
+	}
+  
+  
+}
+
+
