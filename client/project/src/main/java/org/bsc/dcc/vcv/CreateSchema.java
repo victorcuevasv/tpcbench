1baf16264b4fe45ed933153c12b630feb76e2bbb
Your Name <you@example.com>
2020-11-11 17:53:13 +0100

    Add parameter for cluster id.

--- client/project/src/main/java/org/bsc/dcc/vcv/CreateSchema.java
+++ client/project/src/main/java/org/bsc/dcc/vcv/CreateSchema.java
@@ -37,6 +37,7 @@ public class CreateSchema {
 	private final String dbName;
 	private final String clusterId;
 	private final String userId;
+	private final String dbPassword;
 	
 	public CreateSchema(CommandLine commandLine) {
 		this.hostname = commandLine.getOptionValue("server-hostname");
@@ -44,6 +45,7 @@ public class CreateSchema {
 		this.dbName = commandLine.getOptionValue("schema-name");
 		this.clusterId = commandLine.getOptionValue("cluster-id", "UNUSED");
 		this.userId = commandLine.getOptionValue("connection-username", "UNUSED");
+		this.dbPassword = commandLine.getOptionValue("db-password", "UNUSED");
 		this.openConnection();
 	}
 	
@@ -68,6 +70,7 @@ public class CreateSchema {
 		this.dbName = args[2];
 		this.clusterId = "UNUSED";
 		this.userId = "UNUSED";
+		this.dbPassword = "UNUSED";
 		this.openConnection();
 	}
 	
@@ -101,14 +104,13 @@ public class CreateSchema {
 			else if( this.system.equals("databrickssql") ) {
 				// IMPORTANT: HAD TO HARDCODE THIS DUE TO LACK OF PERMISSION TO MANAGE SECRETS.
 				// UPDATE TO PROPER PERMISSIONS WHEN TESTS ARE DONE.
-				String dbrToken = "dapifd4db58404ae64629dc7b41d57f3a769";
 				Class.forName(databricksDriverName);
 				this.con = DriverManager.getConnection("jdbc:spark://"
-					+ this.hostname + ":443/" + this.dbName
-					+ ";transportMode=http;ssl=1;AuthMech=3"
-					+ ";httpPath=/sql/1.0/endpoints/a57e3bc75ae9786b"
-					+ ";UID=token;PWD=" + dbrToken
-					+ ";UseNativeQuery=1");
+						+ this.hostname + ":443/" + this.dbName
+						+ ";transportMode=http;ssl=1;AuthMech=3"
+						+ ";httpPath=/sql/1.0/endpoints/" + this.clusterId
+						+ ";UID=token;PWD=" + this.dbPassword
+						+ ";UseNativeQuery=1");
 			}
 			else if( this.system.equals("redshift") ) {
 				Class.forName(redshiftDriverName);
