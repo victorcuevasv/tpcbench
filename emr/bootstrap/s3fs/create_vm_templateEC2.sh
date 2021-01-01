7d8e67a5538146364be509c79a20c877308e3547
Your Name <you@example.com>
2021-01-01 18:36:17 +0100

    Update script to create vm template.

--- emr/bootstrap/s3fs/create_vm_templateEC2.sh
+++ emr/bootstrap/s3fs/create_vm_templateEC2.sh
@@ -1,5 +1,22 @@
 #!/bin/bash 
 
+#Variables for console output with colors.
+
+red=$'\e[1;31m'
+grn=$'\e[1;32m'
+yel=$'\e[1;33m'
+blu=$'\e[1;34m'
+mag=$'\e[1;35m'
+cyn=$'\e[1;36m'
+end=$'\e[0m'
+
+#$1 Name of the template
+
+if [ $# -lt 1 ]; then
+    echo "${yel}Usage: bash create_vm_templateEC2.sh <template name>${end}"
+    exit 0
+fi
+
 #Obtain json data
 #aws ec2 get-launch-template-data --instance-id "i-0d412d147e820bc7c" --query "LaunchTemplateData" > ec2JSON.txt
 
