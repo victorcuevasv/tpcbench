e57c53c67d5fdf6cd955792644e36937b2764a24
Your Name <you@example.com>
2021-02-06 16:13:27 +0100

    Modify query for merge test.

--- client/project/src/main/resources/QueriesETLTest3/store_sales.sql
+++ client/project/src/main/resources/QueriesETLTest3/store_sales.sql
@@ -1,6 +1,6 @@
 MERGE INTO store_sales_denorm
 USING
-(select i_item_sk from tpcds_sf3000_delta.item where i_color in
+(select i_item_sk from item where i_color in
 ('peach','misty','drab','chocolate','almond','saddle')) s
 on store_sales_denorm.ss_item_sk = s.i_item_sk
 WHEN MATCHED THEN UPDATE SET
