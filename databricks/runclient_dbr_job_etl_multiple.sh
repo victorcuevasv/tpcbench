e00d28dae1cad9caff3e29c3ad238249d9fa2e78
Your Name <you@example.com>
2021-03-08 17:51:55 +0100

    Change runtime version for Databricks.

--- databricks/runclient_dbr_job_etl_multiple.sh
+++ databricks/runclient_dbr_job_etl_multiple.sh
@@ -33,7 +33,7 @@ printf "\n\n%s\n\n" "${mag}Creating the jobs.${end}"
 #Cluster configuration.
 DatabricksHost="dbc-08fc9045-faef.cloud.databricks.com"
 Nodes="2"
-MajorVersion="8"
+MajorVersion="9"
 MinorVersion="p"
 ScalaVersion="x-scala2.12"
 #Note that /dbfs or dbfs: is not included, these are added later.
@@ -55,16 +55,19 @@ post_data_func()
 { 
 	"name":"$JOB_NAME",
 	"new_cluster":{ 
-		"spark_version":"8.x-snapshot-photon-scala2.12",
+		"spark_version":"custom:custom-local__9.x-snapshot-photon-scala2.12__unknown__head__24cc122__99c37f0__jenkins__e098b8a__format-2.lz4",
         "spark_conf":{
-        	"spark.databricks.delta.optimizeWrite.enabled":"true",
-        	"spark.sql.legacy.parquet.int96DatetimeRebaseModeInWrite":"EXCEPTION",
-        	"spark.sql.legacy.parquet.datetimeRebaseModeInWrite":"EXCEPTION",
-        	"spark.databricks.photon.parquetWriter.enabled":"true",
-        	"spark.executor.memory":"8000m",
-			"spark.memory.offHeap.enabled":"true",
+        	"spark.sql.autoBroadcastJoinThreshold":"200000000",
+			"spark.databricks.photon.parquetWriter.enabled":"true",
+			"spark.sql.legacy.parquet.datetimeRebaseModeInWrite":"EXCEPTION",
+			"spark.sql.columnVector.offheap.enabled":"true",
+			"spark.executor.memory":"8000m",
+			"spark.databricks.io.parquet.nativeReader.enabled":"false",
 			"spark.memory.offHeap.size":"36000m",
-			"spark.databricks.photon.enabled":"true"
+			"spark.databricks.photon.enabled":"true",
+			"spark.memory.offHeap.enabled":"true",
+			"spark.sql.legacy.parquet.int96DatetimeRebaseModeInWrite":"EXCEPTION",
+			"spark.databricks.delta.optimizeWrite.enabled":"true"
          },
          "aws_attributes":{ 
             "zone_id":"us-west-2b",
