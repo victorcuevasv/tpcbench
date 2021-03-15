b6e5e3f14061bd863dac3e655a4d79db6140ff3f
Your Name <you@example.com>
2021-03-15 15:05:44 +0100

    Update script for iceberg.

--- emr/runclient_emrspark_createwithstep_iceberg.sh
+++ emr/runclient_emrspark_createwithstep_iceberg.sh
@@ -25,8 +25,7 @@ Version="6.2.0"
 VersionShort="620"
 AutoTerminate="true"
 #Run configuration.
-#Tag="$(date +%s)icebtest"
-Tag="1615797344icebtest"
+Tag="$(date +%s)icebtest"
 ExperimentName="sparkemr-${VersionShort}-${Nodes}nodes-$1gb-$Tag"
 DirNameWarehouse="tpcds-warehouse-sparkemr-${VersionShort}-$1gb-$2-$Tag"
 DirNameResults="sparkemr-${VersionShort}-test"
@@ -80,8 +79,7 @@ args[15]="--number-of-streams=$3"
 # zorder      |zorder update |read test 1 |insupd test    |read test 2    |
 # delete test |read test 3   |gdpr        |read test 4    |power          |
 # tput
-#args[16]="--execution-flags=111111100000111111100"
-args[16]="--execution-flags=000000000000010000000"
+args[16]="--execution-flags=111111100000111111100"
 # count-queries
 args[17]="--count-queries=false"
 # all or denorm table file
