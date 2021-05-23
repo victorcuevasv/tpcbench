a7d2685ac1534a59a4a63e4a9ae5c8f3adfeee23
Your Name <you@example.com>
2021-05-23 21:57:16 +0200

    Fix bug in notebook run script.

--- emr/emrspark_notebook_cli.sh
+++ emr/emrspark_notebook_cli.sh
@@ -1,10 +1,14 @@
 #!/bin/bash   
 
-aws emr --region us-west-2 \
+jsonNotebook=$(aws emr --region us-west-2 \
 start-notebook-execution \
 --editor-id e-EYUEUFEDO1VW655D6L6LNS6KN \
 --relative-path TPCDSLoadTableJarTest.ipynb \
 --notebook-execution-name my-execution-TPCDSLoadTableJarTest \
 --execution-engine '{"Id" : "j-13XAZUVSV5EK5"}' \
---service-role EMR_Notebooks_DefaultRole
+--service-role EMR_Notebooks_DefaultRole)
 
+notebook_exec_id=$(jq -j '.NotebookExecutionId'  <<<  "$jsonNotebook")
+echo "${blu}Running notebook with id ${notebook_exec_id}.${end}"
+	
+	
