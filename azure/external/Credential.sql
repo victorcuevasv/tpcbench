accd39d1f88db965b537876db7c1b74a2147b1d8
Your Name <you@example.com>
2021-06-04 14:07:26 +0200

    Add schemas for azure synapse.

--- azure/external/Credential.sql
+++ azure/external/Credential.sql
@@ -1,5 +1,16 @@
 --Execute as azureuser
+CREATE MASTER KEY ENCRYPTION BY PASSWORD = 'M...F9'
+
+--Execute as azureuser
+--For dedicated pool use only the key
 CREATE DATABASE SCOPED CREDENTIAL [tpcds_credential]
 WITH IDENTITY='SHARED ACCESS SIGNATURE',  
 SECRET = 'odoo...yg=='
+GO
+
+--Execute as azureuser
+--For serverless, use sas token with permissions to read and list
+CREATE DATABASE SCOPED CREDENTIAL [tpcds_credential]
+WITH IDENTITY='SHARED ACCESS SIGNATURE',  
+SECRET = '?sv=2020-02-10&ss=bfqt&srt=...sWiyw15odpToGs8EPbFk%3D'
 GO
