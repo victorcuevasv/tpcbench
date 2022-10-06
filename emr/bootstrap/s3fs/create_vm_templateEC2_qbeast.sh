1a035beeb2f09c9d96b896000884c531906faba4
EC2 Default User <ec2-user@ip-10-0-26-90.ec2.internal>
2022-10-06 13:38:32 +0000

    Add files specific for Qbeast.

---
+++ emr/bootstrap/s3fs/create_vm_templateEC2_qbeast.sh
@@ -0,0 +1,106 @@
+#!/bin/bash 
+
+#Variables for console output with colors.
+
+red=$'\e[1;31m'
+grn=$'\e[1;32m'
+yel=$'\e[1;33m'
+blu=$'\e[1;34m'
+mag=$'\e[1;35m'
+cyn=$'\e[1;36m'
+end=$'\e[0m'
+
+#$1 Name of the template
+
+if [ $# -lt 1 ]; then
+    echo "${yel}Usage: bash create_vm_templateEC2_qbeast.sh <template name>${end}"
+    exit 0
+fi
+
+#Obtain json data
+#aws ec2 get-launch-template-data --instance-id "i-0d412d147e820bc7c" --query "LaunchTemplateData" > ec2JSON.txt
+
+SecurityGroupId="sg-014e2eff525ef62c5"
+RoleWithPolicyArn="arn:aws:iam::834198847803:instance-profile/EMR_EC2_Benchmarking"
+KeyPair="devtest_qbeast_nvirginia_keypair"
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+encodedScript=$(base64 --wrap=0 $DIR/s3fs_init_qbeast.sh)
+
+json_data_func()
+{
+  cat <<EOF
+{
+    "UserData": "$encodedScript", 
+    "Monitoring": {
+        "Enabled": false
+    },
+    "TagSpecifications": [
+        {
+            "ResourceType": "instance", 
+            "Tags": [
+                {
+                    "Value": "BenchmarkVM", 
+                    "Key": "Name"
+                }
+            ]
+        }
+    ], 
+    "CapacityReservationSpecification": {
+        "CapacityReservationPreference": "open"
+    }, 
+    "InstanceInitiatedShutdownBehavior": "stop", 
+    "ImageId": "ami-026b57f3c383c2eec", 
+    "BlockDeviceMappings": [
+        {
+            "DeviceName": "/dev/xvda", 
+            "Ebs": {
+                "Encrypted": false, 
+                "DeleteOnTermination": true, 
+                "VolumeType": "gp2", 
+                "VolumeSize": 20
+            }
+        }
+    ], 
+    "KeyName": "$KeyPair", 
+    "CreditSpecification": {
+        "CpuCredits": "standard"
+    }, 
+    "HibernationOptions": {
+        "Configured": false
+    }, 
+    "EbsOptimized": false, 
+    "Placement": {
+        "Tenancy": "default", 
+        "GroupName": "", 
+        "AvailabilityZone": "us-east-1b"
+    }, 
+    "IamInstanceProfile": {
+        "Arn": "$RoleWithPolicyArn"
+    },
+    "DisableApiTermination": false, 
+    "InstanceType": "t2.medium", 
+    "NetworkInterfaces":[ 
+      { 
+         "DeviceIndex":0,
+         "AssociatePublicIpAddress":true,
+         "Groups":[ 
+            "$SecurityGroupId"
+         ],
+         "DeleteOnTermination":true,
+         "SubnetId": "subnet-02b07eb9167614b28"
+      }
+   ]
+}
+EOF
+}
+
+#Create the template
+aws ec2 create-launch-template \
+--launch-template-name BenchmarkVM \
+--version-description version1 \
+--launch-template-data "$(json_data_func)"
+
+ 
+ 
---
+++ emr/bootstrap/s3fs/s3fs_init_qbeast.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#User to own the directories.
+mUSER="ec2-user"
+#Name of the iam role with the policy enabling access to the s3 buckets.
+iamRoleName="EMR_EC2_Benchmarking_ROLE"
+#Buckets to mount. It is assumed an IAM role enabling access to them is associated with the machine.
+buckets=("benchmarking-jars-1664735376" "benchmarking-results-1664735376")
+
+#Install s3fs.
+sudo yum install -y gcc libstdc++-devel gcc-c++ fuse fuse-devel curl-devel libxml2-devel mailcap automake openssl-devel git
+git clone https://github.com/s3fs-fuse/s3fs-fuse
+sudo amazon-linux-extras install epel -y
+sudo yum install s3fs-fuse -y
+
+#Mount the buckets.
+for bucket in "${buckets[@]}" ; do
+	mkdir /home/$mUSER/$bucket
+	sudo chown $mUSER:$mUSER -R /home/$mUSER/$bucket
+	sudo -u $mUSER s3fs -o iam_role="$iamRoleName" -o url="https://s3.us-east-1.amazonaws.com" \
+	-o endpoint=us-east-1 -o dbglevel=info -o curldbg \
+	-o use_cache=/tmp $bucket /home/$mUSER/$bucket
+done
+
+#Install docker.
+sudo yum install docker -y
+sudo usermod -aG docker ec2-user
+sudo service docker restart
+
+#Install jq
+sudo yum install jq -y
+
+#Install Databricks CLI
+sudo curl -O https://bootstrap.pypa.io/get-pip.py
+sudo -u ec2-user python get-pip.py --user
+sudo rm get-pip.py
+sudo -u ec2-user /home/ec2-user/.local/bin/pip install databricks-cli
+
+
